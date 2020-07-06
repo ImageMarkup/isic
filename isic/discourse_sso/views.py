8e6abc48d7c34a52981e5194563dd26c7f2a5785
Dan LaManna <dan.lamanna@kitware.com>
2020-07-06 15:25:25 -0400

    Use FBV for discourse login

--- isic/discourse_sso/views.py
+++ isic/discourse_sso/views.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from django.conf import settings
 from django.shortcuts import redirect, render
 from django.views.generic.edit import FormView
@@ -9,83 +11,82 @@ import base64
 import urllib
 from django.http import JsonResponse
 
+@dataclass
+class SSOParamSet:
+    sso: bytes
+    sig: str
+    nonce: str
+    return_url: str
 
-class DiscourseSSOLoginView(FormView):
-    template_name = 'login.html'
-    form_class = DiscourseSSOLoginForm
 
-    sso = None
-    sig = None
-    nonce = None
-    success_url = None
+def _get_sso_parameters(request) -> SSOParamSet:
+    sso = request.GET.get('sso')
+    sig = request.GET.get('sig')
 
-    def _setup_request(self, request):
-        self.sso = request.GET.get('sso')
-        self.sig = request.GET.get('sig')
+    if not sig or not sso:
+        raise Exception('Invalid SSO parameters.')
 
-        if not self.sig or not self.sso:
-            return redirect('https://forum.isic-archive.com')
+    sso = sso.encode('utf-8')
 
-        self.sso = self.sso.encode('utf-8')
+    # Extract nonce and return URL
+    qs = base64.b64decode(sso)
+    qs = qs.decode('utf-8')
+    parsed = urllib.parse.parse_qs(qs)
 
-        # Extract nonce and return URL
-        qs = base64.b64decode(self.sso)
-        qs = qs.decode('utf-8')
-        parsed = urllib.parse.parse_qs(qs)
+    if 'nonce' not in parsed or 'return_sso_url' not in parsed:
+        raise Exception('Invalid SSO parameters.')
+    else:
+        nonce = parsed['nonce'][0]
+        return_url = parsed['return_sso_url'][0]
 
-        if 'nonce' not in parsed or 'return_sso_url' not in parsed:
-            return redirect('https://forum.isic-archive.com')
-        else:
-            self.nonce = parsed['nonce'][0]
-            self.success_url = parsed['return_sso_url'][0]
+    # Ensure HMAC-SHA256 digest matches provided signature
+    expected_signature = hmac.new(
+        key=settings.DISCOURSE_SSO_SECRET.encode('utf-8'),
+        msg=sso,
+        digestmod=hashlib.sha256,
+    ).hexdigest()
+    if sig != expected_signature:
+        # TODO: log forged sso
+        raise Exception('Invalid SSO parameters.')
 
-        # Ensure HMAC-SHA256 digest matches provided signature
-        expected_signature = hmac.new(
-            key=settings.DISCOURSE_SSO_SECRET.encode('utf-8'),
-            msg=self.sso,
-            digestmod=hashlib.sha256,
-        ).hexdigest()
-        if self.sig != expected_signature:
-            # TODO: log forged sso
-            return redirect('https://forum.isic-archive.com')
+    return SSOParamSet(sso, sig, nonce, return_url)
 
-    def get(self, request, *args, **kwargs):
-        maybe_redirect = self._setup_request(request)
 
-        if maybe_redirect:
-            return maybe_redirect
+def discourse_sso_login(request):
+    try:
+        sso_params = _get_sso_parameters(request)
+    except Exception:
+        return redirect('https://forum.isic-archive.com')
 
-        return super().get(request, *args, **kwargs)
+    if request.method == 'POST':
+        form = DiscourseSSOLoginForm(request.POST)
 
-    def post(self, request, *args, **kwargs):
-        maybe_redirect = self._setup_request(request)
+        if form.is_valid():
+            payload = {
+                'nonce': sso_params.nonce,
+                'email': form.girder_user['email'],
+                'external_id': str(form.girder_user['_id']),
+                'username': form.girder_user['login'],
+                'name': '%s %s' % (form.girder_user['firstName'], form.girder_user['lastName']),
+                'require_activation': 'false' if form.girder_user['emailVerified'] else 'true',
+                'admin': 'true' if form.girder_user['admin'] else 'false',
+                # Note, this list matches Discourse groups' "name" (which may only include numbers,
+                # letters and underscores), not "Full Name" (which is human readable), so it may be of
+                # limited utility
+                'add_groups': ','.join(group['name'] for group in form.girder_user_groups),
+            }
+            payload = urllib.parse.urlencode(payload)
+            payload = payload.encode('utf-8')
+            payload = base64.b64encode(payload)
 
-        if maybe_redirect:
-            return maybe_redirect
+            digest = hmac.new(
+                key=settings.DISCOURSE_SSO_SECRET.encode('utf-8'), msg=payload, digestmod=hashlib.sha256
+            ).hexdigest()
+            args = urllib.parse.urlencode({'sso': payload, 'sig': digest})
 
-        return super().post(request, *args, **kwargs)
+            return redirect(f'{sso_params.return_url}?{args}')
+    else:
+        form = DiscourseSSOLoginForm()
 
-    def form_valid(self, form: DiscourseSSOLoginForm):
-        payload = {
-            'nonce': self.nonce,
-            'email': form.girder_user['email'],
-            'external_id': str(form.girder_user['_id']),
-            'username': form.girder_user['login'],
-            'name': '%s %s' % (form.girder_user['firstName'], form.girder_user['lastName']),
-            'require_activation': 'false' if form.girder_user['emailVerified'] else 'true',
-            'admin': 'true' if form.girder_user['admin'] else 'false',
-            # Note, this list matches Discourse groups' "name" (which may only include numbers,
-            # letters and underscores), not "Full Name" (which is human readable), so it may be of
-            # limited utility
-            'add_groups': ','.join(group['name'] for group in form.girder_user_groups),
-        }
-        payload = urllib.parse.urlencode(payload)
-        payload = payload.encode('utf-8')
-        payload = base64.b64encode(payload)
+    return render(request, 'login.html', {'form': form})
 
-        digest = hmac.new(
-            key=settings.DISCOURSE_SSO_SECRET.encode('utf-8'), msg=payload, digestmod=hashlib.sha256
-        ).hexdigest()
-        args = urllib.parse.urlencode({'sso': payload, 'sig': digest})
-
-        return redirect(f'{self.success_url}?{args}')
