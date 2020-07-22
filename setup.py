7b217df53620e3f1e65e31ff37a174b33e09eeb7
Brian Helba <brian.helba@kitware.com>
2020-07-22 11:03:40 -0400

    Add missing boto3 requirement

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
         'rich',
         'whitenoise[brotli]',
         # Production-only
-        'django-storages',
+        'django-storages[boto3]',
         'gunicorn',
         'sentry-sdk',
         # Development-only
