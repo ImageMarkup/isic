11d648eca6e9dddfd9f64e5a1f865c907566b142
Brian Helba <brian.helba@kitware.com>
2020-07-22 21:22:13 -0400

    Refactor login app and add testing

--- isic/conftest.py
+++ isic/conftest.py
@@ -1,7 +1,33 @@
+from bson import ObjectId
+import factory
+from passlib.hash import bcrypt
 import pytest
+from pytest_factoryboy import register
 from rest_framework.test import APIClient
 
 
 @pytest.fixture
 def api_client():
     return APIClient()
+
+
+class GirderUserFactory(factory.DictFactory):
+    name = 'foo'
+    _id = factory.LazyFunction(ObjectId)
+    login = factory.Faker('user_name')
+    email = factory.Faker('safe_email')
+    firstName = factory.Faker('first_name')  # noqa: N815
+    lastName = factory.Faker('last_name')  # noqa: N815
+    status = 'enabled'
+    emailVerified = True  # noqa: N815
+    admin = False
+    salt = factory.LazyAttribute(
+        lambda o: bcrypt.using(rounds=4).hash(o.raw_password) if o.raw_password else None
+    )
+    created = factory.Faker('date_time_this_decade')
+
+    class Params:
+        raw_password = factory.Faker('password')
+
+
+register(GirderUserFactory, 'girder_user')
--- isic/login/views.py
+++ isic/login/views.py
@@ -1,36 +1,14 @@
 import json
-from isic.login.girder import get_girder_user
-import datetime
-import random
-import string
-from pymongo.mongo_client import MongoClient
-from django.conf import settings
-from typing import Dict
 
-from oauth2_provider import views as oauth2_views
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
+from oauth2_provider import views as oauth2_views
 from oauth2_provider.models import get_access_token_model
 
-
-def _create_girder_token(girder_user: Dict) -> str:
-    db = MongoClient(settings.ARCHIVE_MONGO_URI).girder
-    now = datetime.datetime.utcnow()
-    token_value = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(64))
-    db['token'].insert_one(
-        {
-            '_id': token_value,
-            'userId': girder_user['_id'],
-            'created': now,
-            'expires': now + datetime.timedelta(days=30.0),
-            'scope': ('core.user_auth',),
-        }
-    )
-    # TODO: What does setUserAccess do?
-    return token_value
+from isic.login.girder import create_girder_token
 
 
-@method_decorator(csrf_exempt, name="dispatch")
+@method_decorator(csrf_exempt, name='dispatch')
 class TokenView(oauth2_views.TokenView):
     def create_token_response(self, request):
         url, headers, body, status = super().create_token_response(request)
@@ -44,7 +22,7 @@ class TokenView(oauth2_views.TokenView):
                     .objects.select_related('user')
                     .get(token=body['access_token'])
                 )
-                body['girder_token'] = _create_girder_token(get_girder_user(token.user.email))
+                body['girder_token'] = create_girder_token(token.user.profile.girder_id)
                 body = json.dumps(body)
 
         return url, headers, body, status
