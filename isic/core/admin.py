6d314d241d3040c9d2ceffdcf85f6f83646795ec
Dan LaManna <dan.lamanna@kitware.com>
2020-12-24 14:17:56 -0500

    Add admin branding/filtering

---
+++ isic/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# general admin settings
+# https://docs.djangoproject.com/en/3.1/ref/contrib/admin/#adminsite-objects
+admin.site.site_header = 'ISIC Admin'
+admin.site.site_title = 'ISIC Admin'
+admin.site.index_title = ''
+
+# TODO: unregister unnecessary apps from admin site
