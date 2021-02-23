03eaf982452ccbeac1837a485dc60021406d02ae
Dan LaManna <dan.lamanna@kitware.com>
2021-02-23 12:40:29 -0500

    Add contributor upload workflow

--- isic/ingest/forms.py
+++ isic/ingest/forms.py
@@ -1,9 +1,21 @@
 from django.forms.models import ModelForm
 
-from isic.ingest.models import Cohort
+from isic.ingest.models import Cohort, Contributor
 
 
 class CohortForm(ModelForm):
     class Meta:
         model = Cohort
-        fields = ['name', 'description']
+        fields = ['contributor', 'name', 'description', 'copyright_license', 'attribution']
+
+
+class ContributorForm(ModelForm):
+    class Meta:
+        model = Contributor
+        fields = [
+            'institution_name',
+            'institution_url',
+            'legal_contact_info',
+            'default_copyright_license',
+            'default_attribution',
+        ]
