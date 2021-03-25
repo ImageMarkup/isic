eb8ffc0ad4c0a9720320eb3286c3d0fb3502bbf1
Brian Helba <brian.helba@kitware.com>
2021-03-25 14:47:12 -0400

    Refactor zip extraction and add tests

--- isic/ingest/tests/test_zip_utils.py
+++ isic/ingest/tests/test_zip_utils.py
@@ -1,33 +1,27 @@
 import zipfile
 
-import pytest
-
 from isic.ingest import zip_utils
 
-
-@pytest.fixture
-def zip_stream(data_dir):
-    with (data_dir / 'ISIC-images.zip').open('rb') as stream:
-        yield stream
+# TODO: Add a more difficult ZIP with skipped content, and use it in these tests
 
 
-def test_file_names_in_zip(zip_stream):
-    file_names = zip_utils.file_names_in_zip(zip_stream)
+def test_file_names_in_zip(zip_stream_only_images):
+    file_names = zip_utils.file_names_in_zip(zip_stream_only_images)
 
     file_names_list = list(file_names)
-    assert len(file_names_list) == 83
+    assert len(file_names_list) == 5
     assert 'ISIC_0000000.jpg' in file_names_list
 
 
-def test_items_in_zip(zip_stream):
-    zip_items = zip_utils.items_in_zip(zip_stream)
+def test_items_in_zip(zip_stream_only_images):
+    zip_items = zip_utils.items_in_zip(zip_stream_only_images)
 
     zip_items_list = list(zip_items)
-    assert len(zip_items_list) == 83
+    assert len(zip_items_list) == 5
 
 
-def test_items_in_zip_item(zip_stream):
-    zip_items = zip_utils.items_in_zip(zip_stream)
+def test_items_in_zip_item(zip_stream_only_images):
+    zip_items = zip_utils.items_in_zip(zip_stream_only_images)
 
     zip_item = next(zip_items)
     assert zip_item.name == 'ISIC_0000000.jpg'
@@ -35,8 +29,8 @@ def test_items_in_zip_item(zip_stream):
     assert zip_item.size == 49982
 
 
-def test_items_in_zip_read(zip_stream):
-    zip_items = zip_utils.items_in_zip(zip_stream)
+def test_items_in_zip_read(zip_stream_only_images):
+    zip_items = zip_utils.items_in_zip(zip_stream_only_images)
     zip_item = next(zip_items)
     zip_item_content = zip_item.stream.read()
 
