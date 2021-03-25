d6f83bc97de4770d176ff016989eabea8612c8cd
Brian Helba <brian.helba@kitware.com>
2021-03-25 14:47:12 -0400

    Refactor zip extraction and add tests

--- isic/ingest/tests/conftest.py
+++ isic/ingest/tests/conftest.py
@@ -1,8 +1,14 @@
-import pathlib
-
 import pytest
+from pytest_factoryboy import register
 
+from .factories import AccessionFactory, CohortFactory, ContributorFactory, ZipFactory
+from .zip_streams import zip_stream_duplicates, zip_stream_only_images
 
-@pytest.fixture
-def data_dir():
-    return pathlib.Path(__file__).parent / 'data'
+register(AccessionFactory)
+register(CohortFactory)
+register(ContributorFactory)
+register(ZipFactory)
+
+# These can't be natively decorated as fixtures, since they're called directly by Factories
+zip_stream_only_images = pytest.fixture(zip_stream_only_images)
+zip_stream_duplicates = pytest.fixture(zip_stream_duplicates)
---
+++ isic/ingest/tests/factories.py
@@ -0,0 +1,55 @@
+import pathlib
+
+import factory
+import factory.django
+
+from isic.factories import UserFactory
+from isic.ingest.models import Accession, Cohort, Contributor, CopyrightLicense, Zip
+
+from .zip_streams import zip_stream_only_images
+
+data_dir = pathlib.Path(__file__).parent / 'data'
+
+
+class ContributorFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Contributor
+
+    creator = factory.SubFactory(UserFactory)
+    institution_url = factory.Faker('url')
+    legal_contact_info = factory.Faker('address')
+
+
+class CohortFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Cohort
+
+    contributor = factory.SubFactory(ContributorFactory)
+    creator = factory.SelfAttribute('contributor.creator')
+    name = factory.Faker('sentence', nb_words=3, variable_nb_words=True)
+    description = factory.Faker('paragraph')
+    copyright_license = CopyrightLicense.CC_BY
+    attribution = factory.Faker('paragraph')
+
+
+class ZipFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Zip
+
+    cohort = factory.SubFactory(CohortFactory)
+    creator = factory.SelfAttribute('cohort.creator')
+    blob = factory.django.FileField(
+        from_func=zip_stream_only_images,
+        filename=factory.Faker('file_name', extension='zip'),
+    )
+    blob_name = factory.SelfAttribute('blob.name')
+    blob_size = factory.SelfAttribute('blob.size')
+
+
+class AccessionFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Accession
+
+    upload = factory.SubFactory(ZipFactory)
+    original_blob = factory.django.FileField(from_path=data_dir / 'ISIC_0000000.jpg')
+    blob_name = factory.SelfAttribute('original_blob.name')
