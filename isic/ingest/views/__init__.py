e1b858ad3c4f1ecfdc790cdf4a2e677efd051cff
Dan LaManna <dan.lamanna@kitware.com>
2021-03-30 13:37:23 -0400

    Refactor view files

--- isic/ingest/views/__init__.py
+++ isic/ingest/views/__init__.py
@@ -14,17 +14,12 @@ import numpy as np
 import pandas as pd
 from pydantic.main import BaseModel
 
-from isic.ingest.forms import CohortForm, ContributorForm
-from isic.ingest.models import (
-    Accession,
-    Cohort,
-    Contributor,
-    DistinctnessMeasure,
-    MetadataFile,
-    Zip,
-)
+from isic.ingest.models import Accession, Cohort, DistinctnessMeasure, MetadataFile, Zip
 from isic.ingest.tasks import apply_metadata as apply_metadata_task, extract_zip
 from isic.ingest.validators import MetadataRow
+
+from .review_apps import *  # noqa
+from .upload import *  # noqa
 
 
 class MetadataFileForm(ModelForm):
@@ -54,7 +49,7 @@ def zip_create(request, cohort_pk):
             form.instance.cohort = cohort
             form.save(commit=True)
             extract_zip.delay(form.instance.id)
-            return HttpResponseRedirect(reverse('cohort-files', args=[cohort.pk]))
+            return HttpResponseRedirect(reverse('upload/cohort-files', args=[cohort.pk]))
     else:
         form = ZipForm()
 
@@ -75,7 +70,7 @@ def metadata_file_create(request, cohort_pk):
             form.instance.blob_name = form.instance.blob.name
             form.instance.cohort = cohort
             form.save(commit=True)
-            return HttpResponseRedirect(reverse('cohort-files', args=[cohort.pk]))
+            return HttpResponseRedirect(reverse('upload/cohort-files', args=[cohort.pk]))
     else:
         form = MetadataFileForm()
 
@@ -335,65 +330,3 @@ def review_skipped_accessions(request, cohort_pk):
         'ingest/review_skipped_accessions.html',
         {'cohort': cohort, 'page_obj': page_obj, 'total_accessions': accessions.count()},
     )
-
-
-@login_required
-def select_or_create_contributor(request):
-    ctx = {'contributors': Contributor.objects.filter(creator=request.user)}
-    if ctx['contributors'].count() == 0:
-        return HttpResponseRedirect(reverse('contributor-create'))
-
-    return render(request, 'ingest/contributor_select_or_create.html', ctx)
-
-
-@login_required
-def select_or_create_cohort(request, contributor_pk):
-    ctx = {
-        'cohorts': Cohort.objects.filter(
-            contributor__creator=request.user, contributor__pk=contributor_pk
-        ),
-        'contributor_pk': contributor_pk,
-    }
-    if ctx['cohorts'].count() == 0:
-        return HttpResponseRedirect(reverse('cohort-create', args=[contributor_pk]))
-
-    return render(request, 'ingest/cohort_select_or_create.html', ctx)
-
-
-@login_required
-def upload_contributor_create(request):
-    if request.method == 'POST':
-        form = ContributorForm(request.POST)
-        if form.is_valid():
-            form.instance.creator = request.user
-            form.save(commit=True)
-            return HttpResponseRedirect(reverse('cohort-create', args=[form.instance.pk]))
-    else:
-        form = ContributorForm()
-
-    return render(request, 'ingest/contributor_create.html', {'form': form})
-
-
-@login_required
-def upload_cohort_create(request, contributor_pk):
-    contributor: Contributor = get_object_or_404(
-        Contributor.objects.filter(creator=request.user), pk=contributor_pk
-    )
-
-    if request.method == 'POST':
-        form = CohortForm(request.POST)
-        if form.is_valid():
-            form.instance.creator = request.user
-            form.instance.contributor = contributor
-            form.save(commit=True)
-            return HttpResponseRedirect(reverse('cohort-files', args=[form.instance.pk]))
-    else:
-        form = CohortForm(
-            initial={
-                'contributor': contributor.pk,
-                'copyright_license': contributor.default_copyright_license,
-                'attribution': contributor.default_attribution,
-            }
-        )
-
-    return render(request, 'ingest/cohort_create.html', {'form': form})
