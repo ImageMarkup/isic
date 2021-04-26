79e916eef28ba29679f57a0f23385c1e2a6418a5
Dan LaManna <dan.lamanna@kitware.com>
2021-04-26 15:52:10 -0400

    Add table for duplicate images

---
+++ isic/core/models.py
@@ -0,0 +1,19 @@
+from django.core.validators import RegexValidator
+from django.db import models
+from django.db.models import JSONField
+from django_extensions.db.models import TimeStampedModel
+
+
+class DuplicateImage(TimeStampedModel):
+    accession = models.ForeignKey(
+        'ingest.Accession', on_delete=models.CASCADE, related_name='duplicates'
+    )
+    girder_id = models.CharField(
+        max_length=24,
+        unique=True,
+        validators=[RegexValidator(r'^[0-9a-f]{24}$')],
+    )
+    isic_id = models.CharField(
+        max_length=12, validators=[RegexValidator(r'^ISIC_[0-9]{7}$')], unique=True
+    )
+    metadata = JSONField(default=dict)
