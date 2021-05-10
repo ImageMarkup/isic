0b4e296e650669b595887e855bb2d5fc28ab9d73
Dan LaManna <dan.lamanna@kitware.com>
2021-05-10 11:07:44 -0400

    Add staff listing page

---
+++ isic/core/views.py
@@ -0,0 +1,9 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
+
+@staff_member_required
+def staff_list(request):
+    users = User.objects.filter(is_staff=True).order_by('email')
+    return render(request, 'core/staff_list.html', {'users': users})
