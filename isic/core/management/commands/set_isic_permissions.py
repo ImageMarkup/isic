f65698d5b2075611e48a481bcd22151e7b2e567a
Dan LaManna <dan.lamanna@kitware.com>
2021-05-10 14:07:08 -0400

    Add script for staff and permissions

---
+++ isic/core/management/commands/set_isic_permissions.py
@@ -0,0 +1,57 @@
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.contenttypes.models import ContentType
+import djclick as click
+
+from isic.core.models import DuplicateImage
+from isic.ingest.models import (
+    Accession,
+    CheckLog,
+    Cohort,
+    Contributor,
+    DistinctnessMeasure,
+    MetadataFile,
+    Zip,
+)
+from isic.studies.models import (
+    Annotation,
+    Feature,
+    Markup,
+    Question,
+    QuestionChoice,
+    Response,
+    Study,
+    StudyTask,
+)
+
+
+@click.command(help='Add ISIC Staff group with basic permissions')
+def add_staff_group():
+    group, _ = Group.objects.get_or_create(name='ISIC Staff')
+
+    for model in [
+        Accession,
+        Annotation,
+        CheckLog,
+        Cohort,
+        Contributor,
+        DistinctnessMeasure,
+        DuplicateImage,
+        Feature,
+        Markup,
+        MetadataFile,
+        Question,
+        QuestionChoice,
+        Response,
+        Study,
+        StudyTask,
+        User,
+        Zip,
+    ]:
+        content_type = ContentType.objects.get_for_model(model)
+        for permission in ['view', 'change']:
+            group.permissions.add(
+                Permission.objects.get(
+                    codename=f'{permission}_{content_type.model}',
+                    content_type=content_type,
+                )
+            )
