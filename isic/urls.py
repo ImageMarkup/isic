7dd394680ebce34e81bba4d436807b11f7fe8937
Dan LaManna <dan.lamanna@kitware.com>
2021-05-12 12:58:34 -0400

    Split urls across apps

--- isic/urls.py
+++ isic/urls.py
@@ -10,27 +10,8 @@ from rest_framework import permissions, routers
 from isic.core.views import staff_list
 from isic.discourse_sso.views import discourse_sso_login
 from isic.ingest.api import AccessionViewSet, MetadataFileViewSet
-from isic.ingest.views import (
-    DiagnosisReviewAppView,
-    DuplicateReviewAppView,
-    LesionReviewAppView,
-    QualityPhiReviewAppView,
-    apply_metadata,
-    cohort_detail,
-    cohort_files,
-    cohort_list,
-    ingest_review,
-    metadata_file_create,
-    reset_metadata,
-    select_or_create_cohort,
-    select_or_create_contributor,
-    upload_cohort_create,
-    upload_contributor_create,
-    zip_create,
-)
 from isic.login.views import IsicLoginView, get_girder_token
 from isic.studies.api import AnnotationViewSet, StudyTaskViewSet, StudyViewSet
-from isic.studies.views import annotation_detail, study_create, study_detail, study_list, view_mask
 
 router = routers.SimpleRouter()
 router.register('accessions', AccessionViewSet)
@@ -62,60 +43,8 @@ urlpatterns = [
         'staff/', TemplateView.as_view(template_name='core/staff_landing.html'), name='staff-index'
     ),
     path('staff/users/', staff_list, name='staff-list'),
-    # Studies app
-    path('staff/studies/create/', study_create, name='study-create'),
-    path('staff/studies/', study_list, name='study-list'),
-    path('staff/studies/<pk>/', study_detail, name='study-detail'),
-    path('staff/masks/<markup_id>/', view_mask, name='view-mask'),
-    path('staff/annotations/<pk>/', annotation_detail, name='annotation-detail'),
-    # Ingest app
-    path(
-        'upload/select-or-create-contributor/',
-        select_or_create_contributor,
-        name='upload/select-or-create-contributor',
-    ),
-    path('upload/create-contributor/', upload_contributor_create, name='upload/create-contributor'),
-    path(
-        'upload/select-or-create-cohort/<contributor_pk>/',
-        select_or_create_cohort,
-        name='upload/select-or-create-cohort',
-    ),
-    path(
-        'upload/create-cohort/<contributor_pk>', upload_cohort_create, name='upload/create-cohort'
-    ),
-    path('upload/<pk>/files/', cohort_files, name='upload/cohort-files'),
-    path('cohort/<cohort_pk>/upload-zip/', zip_create, name='upload-zip'),
-    path('cohort/<cohort_pk>/upload-metadata/', metadata_file_create, name='upload-metadata'),
-    # Staff pges
-    path('staff/ingest-review/', ingest_review, name='ingest-review'),
-    path('staff/cohorts/', cohort_list, name='cohort-list'),
-    path('staff/ingest-review/<pk>/', cohort_detail, name='cohort-detail'),
-    path(
-        'staff/ingest-review/diagnosis/<cohort_pk>/',
-        DiagnosisReviewAppView.as_view(),
-        name='cohort-review-diagnosis',
-    ),
-    path(
-        'staff/ingest-review/quality-and-phi/<cohort_pk>/',
-        QualityPhiReviewAppView.as_view(),
-        name='cohort-review-quality-and-phi',
-    ),
-    path(
-        'staff/ingest-review/duplicate/<cohort_pk>/',
-        DuplicateReviewAppView.as_view(),
-        name='cohort-review-duplicate',
-    ),
-    path(
-        'staff/ingest-review/lesion/<cohort_pk>/',
-        LesionReviewAppView.as_view(),
-        name='cohort-review-lesion',
-    ),
-    path(
-        'staff/ingest-review/<cohort_pk>/validate-metadata/',
-        apply_metadata,
-        name='validate-metadata',
-    ),
-    path('staff/reset-metadata/<cohort_pk>/', reset_metadata, name='reset-metadata'),
+    path('', include('isic.ingest.urls')),
+    path('', include('isic.studies.urls')),
 ]
 
 if apps.is_installed('isic.discourse_sso'):
