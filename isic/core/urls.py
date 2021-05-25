6a448174bf556fe63b474c57975581ae86ee5b87
Dan LaManna <dan.lamanna@kitware.com>
2021-05-24 22:49:22 -0400

    Add collection detail page

--- isic/core/urls.py
+++ isic/core/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from isic.core.views import image_detail
+from isic.core.views import collection_detail, image_detail
 
 urlpatterns = [
     path(
         'images/<id_or_gid_or_isicid>/',
         image_detail,
         name='core/image-detail',
+    ),
+    path(
+        'collections/<pk>/',
+        collection_detail,
+        name='core/collection-detail',
     ),
 ]
