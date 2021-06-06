b0258643e1f60522bc198f96951885f4f7bbb887
Dan LaManna <dan.lamanna@kitware.com>
2021-06-06 17:44:08 -0400

    Solidify permissions testing

--- isic/ingest/tests/test_permissions.py
+++ isic/ingest/tests/test_permissions.py
@@ -3,11 +3,16 @@ import pytest
 
 
 @pytest.mark.django_db
-def test_upload_select_contributor_permissions(client, staff_client, contributor_factory):
+def test_upload_select_contributor_permissions(
+    client, authenticated_client, staff_client, contributor_factory
+):
     c1, c2, c3 = contributor_factory(), contributor_factory(), contributor_factory()
 
     r = client.get(reverse('upload/select-or-create-contributor'))
-    assert r.status_code == 302
+    assert r.status_code == 302  # redirect to login page
+
+    r = authenticated_client.get(reverse('upload/select-or-create-contributor'))
+    assert r.status_code == 302  # redirect to create-contributor page
 
     client.force_login(c1.creator)
     r = client.get(reverse('upload/select-or-create-contributor'))
@@ -17,6 +22,53 @@ def test_upload_select_contributor_permissions(client, staff_client, contributor
     r = staff_client.get(reverse('upload/select-or-create-contributor'))
     assert r.status_code == 200
     assert set(r.context['contributors'].values_list('pk', flat=True)) == {c1.pk, c2.pk, c3.pk}
+
+
+@pytest.mark.django_db
+def test_upload_create_contributor_permissions(client, authenticated_client):
+    r = client.get(reverse('upload/create-contributor'))
+    assert r.status_code == 302  # redirect to login page
+
+    r = authenticated_client.get(reverse('upload/create-contributor'))
+    assert r.status_code == 200
+
+
+@pytest.mark.django_db
+def test_upload_select_create_cohort_permissions(
+    client, authenticated_client, staff_client, cohort_factory, contributor_factory
+):
+    contributor1, contributor2 = contributor_factory(), contributor_factory()
+    cohort_factory(contributor=contributor1)
+    cohort_factory(contributor=contributor2)
+
+    for contributor in [contributor1, contributor2]:
+        r = client.get(reverse('upload/select-or-create-cohort', args=[contributor.pk]))
+        assert r.status_code == 302  # redirect to login page
+
+        r = authenticated_client.get(
+            reverse('upload/select-or-create-cohort', args=[contributor.pk])
+        )
+        assert r.status_code == 302  # redirect to create-cohort page
+
+        client.force_login(contributor.creator)
+        r = client.get(reverse('upload/select-or-create-cohort', args=[contributor.pk]))
+        assert r.status_code == 200
+        assert set(r.context['cohorts']) == set(list(contributor.cohorts.all()))
+
+        r = staff_client.get(reverse('upload/select-or-create-cohort', args=[contributor.pk]))
+        assert r.status_code == 200
+        assert set(r.context['cohorts']) == set(list(contributor.cohorts.all()))
+
+
+@pytest.mark.django_db
+def test_upload_create_cohort_permissions(client, authenticated_client, contributor_factory, user):
+    contributor = contributor_factory(creator=user)
+
+    r = client.get(reverse('upload/create-cohort', args=[contributor.pk]))
+    assert r.status_code == 302  # redirect to login page
+
+    r = authenticated_client.get(reverse('upload/create-cohort', args=[contributor.pk]))
+    assert r.status_code == 200
 
 
 @pytest.mark.django_db
@@ -82,4 +134,43 @@ def test_cohort_review_permissions(url_name, client, authenticated_client, staff
     assert r.status_code == 302
 
     r = staff_client.get(reverse(url_name, args=[cohort.pk]))
+    assert r.status_code == 200
+
+
+@pytest.mark.django_db
+def test_reset_metadata_permissions(client, authenticated_client, staff_client, cohort):
+    r = client.get(reverse('reset-metadata', args=[cohort.pk]))
+    assert r.status_code == 302
+
+    client.force_login(cohort.contributor.creator)
+    r = client.get(reverse('reset-metadata', args=[cohort.pk]))
+    assert r.status_code == 302
+
+    r = authenticated_client.get(reverse('reset-metadata', args=[cohort.pk]))
+    assert r.status_code == 302
+
+    # TODO: how to handle redirects generally since they're a bad indicator for 'access denied'
+    # in tests
+    r = staff_client.get(reverse('reset-metadata', args=[cohort.pk]))
+    assert r.status_code == 302
+    # 302s on a successful page load, so test where it's redirecting as a proxy for access
+    assert r.url == reverse('cohort-detail', args=[cohort.pk])
+
+
+@pytest.mark.django_db
+def test_validate_metadata_permissions(client, authenticated_client, staff_client, cohort):
+    r = client.get(reverse('validate-metadata', args=[cohort.pk]))
+    assert r.status_code == 302
+
+    client.force_login(cohort.contributor.creator)
+    r = client.get(reverse('validate-metadata', args=[cohort.pk]))
+    assert r.status_code == 302
+
+    r = authenticated_client.get(reverse('validate-metadata', args=[cohort.pk]))
+    assert r.status_code == 302
+
+    r = staff_client.get(reverse('validate-metadata', args=[cohort.pk]))
+    assert r.status_code == 200
+
+    r = staff_client.get(reverse('validate-metadata', args=[cohort.pk]))
     assert r.status_code == 200
