d8a146e2881f2c94ad2f242227bfb3a84b7ddecf
Brian Helba <brian.helba@kitware.com>
2021-06-09 15:10:42 -0400

    Make Profile.girder_salt non-nullable

---
+++ isic/login/migrations/0004_user_profile.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+
+
+def ensure_profile(apps, schema_editor):
+    """Add missing Profiles, due to a bug where they were sometimes not created."""
+    User = apps.get_model('auth', 'User')  # noqa: N806
+    Profile = apps.get_model('login', 'Profile')  # noqa: N806
+
+    for user in User.objects.filter(profile=None):
+        Profile.objects.create(user=user)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('login', '0004_auto_20210609_1909'),
+    ]
+
+    operations = [migrations.RunPython(ensure_profile)]
