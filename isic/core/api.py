1115d28a08259ec21fe8ee7b473d7e1f09f8f680
Dan LaManna <dan.lamanna@kitware.com>
2021-08-04 16:37:29 -0400

    Add elasticsearch implementation

--- isic/core/api.py
+++ isic/core/api.py
@@ -1,12 +1,16 @@
+from typing import Optional
+
+from django.contrib.auth.models import User
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from isic.core.models.image import Image
 from isic.core.permissions import IsicObjectPermissionsFilter
-from isic.core.serializers import ImageSerializer
+from isic.core.search import facets, search_images
+from isic.core.serializers import ImageSerializer, SearchQuerySerializer
 from isic.core.stats import get_archive_stats
 
 
@@ -19,6 +23,19 @@ def stats(request):
     return Response(get_archive_stats())
 
 
+def build_filtered_query(user: User, query: Optional[str] = None) -> dict:
+    query_dict = {'bool': {}}
+
+    if query:
+        query_dict['bool']['must'] = {'query_string': {'query': query}}
+
+    if not user.is_staff:
+        # filter has the same conditions as must but can also cache the result of the filter.
+        query_dict['bool']['filter'] = {'term': {'public': 'true'}}
+
+    return query_dict
+
+
 class ImageViewSet(ReadOnlyModelViewSet):
     serializer_class = ImageSerializer
     queryset = (
@@ -26,3 +43,37 @@ class ImageViewSet(ReadOnlyModelViewSet):
     )
     filter_backends = [IsicObjectPermissionsFilter]
     lookup_field = 'isic_id'
+
+    @swagger_auto_schema(
+        operation_description='Retrieve the facet counts of a query.',
+        query_serializer=SearchQuerySerializer,
+    )
+    @action(detail=False, methods=['get'], pagination_class=None)
+    def facets(self, request):
+        serializer = SearchQuerySerializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+        query = build_filtered_query(request.user, serializer.data.get('query'))
+        return Response(facets(query))
+
+    @swagger_auto_schema(
+        operation_description='Search images with a key:value query string.',
+        query_serializer=SearchQuerySerializer,
+    )
+    @action(detail=False, methods=['get'])
+    def search(self, request):
+        serializer = SearchQuerySerializer(data=request.query_params)
+        serializer.is_valid(raise_exception=True)
+        query = build_filtered_query(request.user, serializer.data.get('query'))
+        search_results = search_images(
+            query, self.paginator.get_limit(request), self.paginator.get_offset(request)
+        )
+        isic_ids = [x['fields']['id'][0] for x in search_results['hits']['hits']]
+        images = self.get_queryset().filter(pk__in=isic_ids)
+        page = self.paginate_queryset(images)
+        serializer = self.get_serializer(page, many=True)
+        paginated_response = self.get_paginated_response(serializer.data)
+
+        # The count needs to be overriden otherwise the paginator tries to
+        # get a count for the queryset (images), which will always be PAGE_SIZE.
+        paginated_response.data['count'] = search_results['hits']['total']['value']
+        return paginated_response
---
+++ isic/core/search.py
@@ -0,0 +1,168 @@
+from functools import lru_cache
+import logging
+from typing import Optional
+
+from django.conf import settings
+from django.db.models.query import QuerySet
+from elasticsearch.client import Elasticsearch
+from elasticsearch.helpers.actions import streaming_bulk
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_SEARCH_AGGREGATES = {
+    'diagnosis': {'terms': {'field': 'diagnosis'}},
+    'age_approx': {
+        'histogram': {
+            'field': 'age_approx',
+            'interval': 10,
+            'extended_bounds': {'min': 0, 'max': 100},
+        }
+    },
+    'sex': {'terms': {'field': 'sex'}},
+    'benign_malignant': {'terms': {'field': 'benign_malignant'}},
+    'diagnosis_confirm_type': {'terms': {'field': 'diagnosis_confirm_type'}},
+    'personal_hx_mm': {'terms': {'field': 'personal_hx_mm'}},
+    'family_hx_mm': {'terms': {'field': 'family_hx_mm'}},
+    'clin_size_long_diam_mm': {
+        'histogram': {
+            'field': 'clin_size_long_diam_mm',
+            'interval': 10,
+            'extended_bounds': {'min': 0, 'max': 110},
+        }
+    },
+    'melanocytic': {'terms': {'field': 'melanocytic'}},
+    'patient_id': {'terms': {'field': 'patient_id'}},
+    'lesion_id': {'terms': {'field': 'lesion_id'}},
+    'acquisition_day': {'terms': {'field': 'acquisition_day'}},
+    'marker_pen': {'terms': {'field': 'marker_pen'}},
+    'hairy': {'terms': {'field': 'hairy'}},
+    'blurry': {'terms': {'field': 'blurry'}},
+    'nevus_type': {'terms': {'field': 'nevus_type'}},
+    'image_type': {'terms': {'field': 'image_type'}},
+    'dermoscopic_type': {'terms': {'field': 'dermoscopic_type'}},
+    'anatom_site_general': {'terms': {'field': 'anatom_site_general'}},
+    'color_tint': {'terms': {'field': 'color_tint'}},
+    'mel_class': {'terms': {'field': 'mel_class'}},
+    'mel_mitotic_index': {'terms': {'field': 'mel_mitotic_index'}},
+    'mel_thick_mm': {
+        'range': {
+            'field': 'mel_thick_mm',
+            'ranges': [
+                {'from': 0.0, 'to': 0.5},
+                {'from': 0.5, 'to': 1.0},
+                {'from': 1.0, 'to': 1.5},
+                {'from': 1.5, 'to': 2.0},
+                {'from': 2.0, 'to': 2.5},
+                {'from': 2.5, 'to': 3.0},
+                {'from': 3.0, 'to': 3.5},
+                {'from': 3.5, 'to': 4.0},
+                {'from': 4.0, 'to': 4.5},
+                {'from': 4.5, 'to': 5.0},
+                {'from': 5.0},
+            ],
+        }
+    },
+    'mel_type': {'terms': {'field': 'mel_type'}},
+    'mel_ulcer': {'terms': {'field': 'mel_ulcer'}},
+}
+
+
+@lru_cache
+def get_elasticsearch_client():
+    return Elasticsearch(settings.ISIC_ELASTICSEARCH_URI)
+
+
+def maybe_create_index():
+    # TODO: include private meta fields (e.g. patient/lesion id)
+    get_elasticsearch_client().indices.create(
+        index=settings.ISIC_ELASTICSEARCH_INDEX,
+        ignore=400,  # ignore status code 400 (index already exists)
+        body={
+            'mappings': {
+                'properties': {
+                    'created': {'type': 'date'},
+                    # TODO: unique index name?
+                    'name': {'type': 'text'},
+                    'public': {'type': 'boolean'},
+                    'age_approx': {'type': 'integer'},
+                    'sex': {'type': 'keyword'},
+                    'benign_malignant': {'type': 'keyword'},
+                    'diagnosis': {'type': 'keyword'},
+                    'diagnosis_confirm_type': {'type': 'keyword'},
+                    'personal_hx_mm': {'type': 'boolean'},
+                    'family_hx_mm': {'type': 'boolean'},
+                    'clin_size_long_diam_mm': {'type': 'float'},
+                    'melanocytic': {'type': 'boolean'},
+                    'acquisition_day': {'type': 'float'},
+                    'marker_pen': {'type': 'boolean'},
+                    'hairy': {'type': 'boolean'},
+                    'blurry': {'type': 'boolean'},
+                    'nevus_type': {'type': 'keyword'},
+                    'image_type': {'type': 'keyword'},
+                    'dermoscopic_type': {'type': 'keyword'},
+                    'anatom_site_general': {'type': 'keyword'},
+                    'color_tint': {'type': 'keyword'},
+                    'mel_class': {'type': 'keyword'},
+                    'mel_mitotic_index': {'type': 'keyword'},
+                    'mel_thick_mm': {'type': 'float'},
+                    'mel_type': {'type': 'keyword'},
+                    'mel_ulcer': {'type': 'boolean'},
+                }
+            }
+        },
+    )
+
+
+def add_to_search_index(image) -> None:
+    get_elasticsearch_client().index(
+        index=settings.ISIC_ELASTICSEARCH_INDEX, body=image.as_elasticsearch_document
+    )
+
+
+def bulk_add_to_search_index(qs: QuerySet) -> None:
+    # Use a generator for lazy evaluation
+    image_documents = (
+        image.as_elasticsearch_document for image in qs.select_related('accession').all()
+    )
+
+    for success, info in streaming_bulk(
+        client=get_elasticsearch_client(),
+        index=settings.ISIC_ELASTICSEARCH_INDEX,
+        actions=image_documents,
+    ):
+        if not success:
+            logger.error('Failed to insert document into elasticsearch', info)
+
+
+def facets(query: Optional[dict] = None) -> dict:
+    body = {
+        'size': 0,
+        'aggs': DEFAULT_SEARCH_AGGREGATES,
+    }
+
+    if query:
+        body['query'] = query
+
+    return get_elasticsearch_client().search(index=settings.ISIC_ELASTICSEARCH_INDEX, body=body)[
+        'aggregations'
+    ]
+
+
+def search_images(
+    query: Optional[dict] = None,
+    limit: Optional[int] = settings.REST_FRAMEWORK['PAGE_SIZE'],
+    offset: Optional[int] = 0,
+) -> dict:
+    # TODO: stably sort by _created
+    body = {
+        'from': offset,
+        'size': limit,
+        'fields': ['id'],
+        '_source': False,
+        'track_total_hits': True,
+    }
+
+    if query:
+        body['query'] = query
+
+    return get_elasticsearch_client().search(index=settings.ISIC_ELASTICSEARCH_INDEX, body=body)
