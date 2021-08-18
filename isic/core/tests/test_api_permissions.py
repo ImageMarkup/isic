53c65e025c0109af745765b65548ba2b329e4db1
Dan LaManna <dan.lamanna@kitware.com>
2021-08-18 11:31:48 -0400

    Add image share testing

--- isic/core/tests/test_api_permissions.py
+++ isic/core/tests/test_api_permissions.py
@@ -9,6 +9,11 @@ def images(image_factory):
     ]
 
 
+@pytest.fixture
+def private_image(image_factory):
+    return image_factory(public=False)
+
+
 @pytest.mark.django_db
 def test_core_api_image_list(images, api_client, authenticated_api_client, staff_api_client):
     for client in [api_client, authenticated_api_client]:
@@ -20,6 +25,32 @@ def test_core_api_image_list(images, api_client, authenticated_api_client, staff
     r = staff_api_client.get('/api/v2/images/')
     assert r.status_code == 200, r.data
     assert r.data['count'] == 2
+
+
+@pytest.mark.django_db
+def test_core_api_image_list_private(private_image, authenticated_api_client):
+    r = authenticated_api_client.get('/api/v2/images/')
+    assert r.status_code == 200, r.data
+    assert r.data['count'] == 0
+
+
+@pytest.mark.django_db
+def test_core_api_image_list_contributed(private_image, authenticated_api_client, user):
+    private_image.accession.upload.cohort.contributor.owners.add(user)
+
+    r = authenticated_api_client.get('/api/v2/images/')
+    assert r.status_code == 200, r.data
+    assert r.data['count'] == 1
+
+
+@pytest.mark.django_db
+def test_core_api_image_list_shares(private_image, authenticated_api_client, user):
+    private_image.shares.add(user, through_defaults={'creator': user})
+    private_image.save()
+
+    r = authenticated_api_client.get('/api/v2/images/')
+    assert r.status_code == 200, r.data
+    assert r.data['count'] == 1
 
 
 @pytest.mark.django_db
