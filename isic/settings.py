3e0ee0e3dbb564bbb5b348b95546eabb56880928
Dan LaManna <dan.lamanna@kitware.com>
2021-10-28 22:29:48 -0400

    Rename before_binding->mutate_configuration

--- isic/settings.py
+++ isic/settings.py
@@ -29,7 +29,7 @@ class IsicMixin(ConfigMixin):
     BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
     @staticmethod
-    def before_binding(configuration: ComposedConfiguration) -> None:
+    def mutate_configuration(configuration: ComposedConfiguration) -> None:
         # Install local apps first, to ensure any overridden resources are found first
         configuration.INSTALLED_APPS = [
             'isic.core.apps.CoreConfig',
