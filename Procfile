e60c1c92fc05dd4b346ba1eb30081cd0c2355f74
Dan LaManna <dan.lamanna@kitware.com>
2021-11-16 12:33:52 -0500

    Refactor isic permissions script

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-release: ./manage.py migrate
+release: ./manage.py migrate && ./manage.py set_isic_permissions
 web: gunicorn --bind 0.0.0.0:$PORT isic.wsgi
 worker: REMAP_SIGTERM=SIGQUIT celery --app isic.celery worker --loglevel INFO --without-heartbeat --concurrency 2
--- isic/core/management/commands/set_isic_permissions.py
+++ isic/core/management/commands/set_isic_permissions.py
@@ -1,60 +1,20 @@
-from django.contrib.auth.models import Group, Permission, User
+import django.apps
+from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
 import djclick as click
 
-from isic.core.models import Collection, DuplicateImage, Image, ImageAlias
-from isic.ingest.models import (
-    Accession,
-    CheckLog,
-    Cohort,
-    Contributor,
-    DistinctnessMeasure,
-    MetadataFile,
-    ZipUpload,
-)
-from isic.studies.models import (
-    Annotation,
-    Feature,
-    Markup,
-    Question,
-    QuestionChoice,
-    Response,
-    Study,
-    StudyTask,
-)
 
-
-@click.command(help='Add ISIC Staff group with basic permissions')
+@click.command(help='Add ISIC Staff group with basic view permissions')
 def add_staff_group():
     group, _ = Group.objects.get_or_create(name='ISIC Staff')
 
-    for model in [
-        Accession,
-        Annotation,
-        CheckLog,
-        Cohort,
-        Collection,
-        Contributor,
-        DistinctnessMeasure,
-        DuplicateImage,
-        Feature,
-        Image,
-        ImageAlias,
-        Markup,
-        MetadataFile,
-        Question,
-        QuestionChoice,
-        Response,
-        Study,
-        StudyTask,
-        User,
-        ZipUpload,
-    ]:
-        content_type = ContentType.objects.get_for_model(model)
-        for permission in ['view', 'change']:
-            group.permissions.add(
-                Permission.objects.get(
-                    codename=f'{permission}_{content_type.model}',
-                    content_type=content_type,
+    for model in django.apps.apps.get_models():
+        if model.__module__.startswith('isic.'):  # TODO: hacky?
+            content_type = ContentType.objects.get_for_model(model)
+            for permission in ['view']:
+                group.permissions.add(
+                    Permission.objects.get(
+                        codename=f'{permission}_{content_type.model}',
+                        content_type=content_type,
+                    )
                 )
-            )
