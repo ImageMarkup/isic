cb044663c31d56dd91c6b91d3d6372eb169c6cb1
Dan LaManna <dan.lamanna@kitware.com>
2021-12-07 23:17:14 -0500

    Add hashid implementation

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'django-object-actions',
         'djangorestframework',
         'drf-yasg',
+        'hashids',
         'opensearch-py',
         'pandas',
         'passlib[bcrypt]',
