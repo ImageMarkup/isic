4671738760dd4576f902de168a666a2160323477
Dan LaManna <dan.lamanna@kitware.com>
2022-01-31 16:47:40 -0500

    Add oauth scope test

--- isic/conftest.py
+++ isic/conftest.py
@@ -1,4 +1,6 @@
 from django.test.client import Client
+from django.utils import timezone
+from oauth2_provider.models import get_access_token_model, get_application_model
 import pytest
 from pytest_factoryboy import register
 from rest_framework.test import APIClient
@@ -63,6 +65,34 @@ def staff_api_client(staff_user) -> APIClient:
     api_client = APIClient()
     api_client.force_authenticate(user=staff_user)
     return api_client
+
+
+@pytest.fixture
+def oauth_app(user_factory):
+    user = user_factory()
+    return get_application_model().objects.create(
+        name='Test Application',
+        redirect_uris='http://localhost',
+        user=user,
+        client_type=get_application_model().CLIENT_CONFIDENTIAL,
+        authorization_grant_type=get_application_model().GRANT_AUTHORIZATION_CODE,
+    )
+
+
+@pytest.fixture
+def oauth_token_client(api_client, oauth_app):
+    def f(user, scope='identity'):  # TODO: settings default scope
+        token = get_access_token_model().objects.create(
+            user=user,
+            scope=scope,
+            expires=timezone.now() + timezone.timedelta(seconds=300),
+            token='some-token',
+            application=oauth_app,
+        )
+        api_client.credentials(Authorization=f'Bearer {token}')
+        return api_client
+
+    return f
 
 
 # To make pytest-factoryboy fixture creation work properly, all factories must be registered at
