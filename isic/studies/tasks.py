eb15cb4a277c53dc6e7288b7d35f782f931dba4c
Dan LaManna <dan.lamanna@kitware.com>
2022-02-23 09:52:42 -0500

    Add form for creating a study

--- isic/studies/tasks.py
+++ isic/studies/tasks.py
@@ -1,13 +1,18 @@
 from celery import shared_task
+from django.db import transaction
 
-from isic.studies.models import StudyTask
+from isic.studies.models import Study, StudyTask
 
 
 @shared_task
-def create_study_task(study_pk, annotator_pk, image_pk):
-    # For efficiency, set the _id fields directly, instead of loading ForeignKey references
-    StudyTask.objects.create(
-        study_id=study_pk,
-        annotator_id=annotator_pk,
-        image_id=image_pk,
-    )
+def populate_study_tasks_task(study_pk: int, user_pks: list[int]):
+    study = Study.objects.prefetch_related('collection__images').get(pk=study_pk)
+
+    with transaction.atomic():
+        for image_pk in study.collection.images.values_list('pk', flat=True):
+            for user_pk in user_pks:
+                StudyTask.objects.create(
+                    study_id=study_pk,
+                    annotator_id=user_pk,
+                    image_id=image_pk,
+                )
