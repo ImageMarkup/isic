844e7d09867cc38f4831b150906d8e2898172a43
Dan LaManna <dan.lamanna@kitware.com>
2022-02-26 13:26:08 -0500

    Add ga metrics to stats endpoint

--- isic/stats/views.py
+++ isic/stats/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 
 from isic.core.models import Collection, Image
 from isic.ingest.models import Contributor, ZipUpload
+from isic.stats.models import GaMetrics
 from isic.studies.models import Annotation, Markup, Response, Study
 
 
 def get_archive_stats():
+    latest_ga_metrics = GaMetrics.objects.last()
+
     ctx = {
         'annotations_count': Annotation.objects.count(),
         'collections_count': Collection.objects.count(),
@@ -18,43 +21,54 @@ def get_archive_stats():
         'responses_count': Response.objects.count(),
         'studies_count': Study.objects.count(),
         'annotated_images_count': Annotation.objects.values('image').distinct().count(),
+        # TODO: keep?
         'uploaders_count': ZipUpload.objects.values('creator').distinct().count(),
         'annotating_users_count': Annotation.objects.values('annotator').distinct().count(),
         'total_users_count': User.objects.count(),
     }
 
+    if latest_ga_metrics:
+        ctx['sessions_last_30_days_count'] = latest_ga_metrics.num_sessions
+        ctx['sessions_last_30_days_top_10_country_count'] = latest_ga_metrics.top_countries(10)
+    else:
+        ctx['sessions_last_30_days_count'] = 0
+        ctx['sessions_last_30_days_top_10_country_count'] = []
+
     return ctx
 
 
 def stats(request):
-    ctx = {}
     archive_stats = get_archive_stats()
-
-    ctx['stats'] = [
-        [
-            ('Users', archive_stats['total_users_count']),
-            ('Uploading Users', archive_stats['uploaders_count']),
-            ('Annotating Users', archive_stats['annotating_users_count']),
+    ctx = {
+        'sessions_last_30_days_top_10_country_count': archive_stats[
+            'sessions_last_30_days_top_10_country_count'
         ],
-        [
-            ('Contributors', archive_stats['contributors_count']),
-            ('', ''),
-            ('Collections', archive_stats['collections_count']),
+        'stats': [
+            [
+                ('Users', archive_stats['total_users_count']),
+                ('Sessions (Last 30 days)', archive_stats['sessions_last_30_days_count']),
+                ('Annotating Users', archive_stats['annotating_users_count']),
+            ],
+            [
+                ('Contributors', archive_stats['contributors_count']),
+                ('', ''),
+                ('Collections', archive_stats['collections_count']),
+            ],
+            [
+                ('Images', archive_stats['images_count']),
+                ('Public Images', archive_stats['public_images_count']),
+                ('Annotated Images', archive_stats['annotated_images_count']),
+            ],
+            [
+                ('Studies', archive_stats['studies_count']),
+                ('Public Studies', archive_stats['public_studies_count']),
+                ('', ''),
+            ],
+            [
+                ('Annotations', archive_stats['annotations_count']),
+                ('Markups', archive_stats['markups_count']),
+                ('Responses', archive_stats['responses_count']),
+            ],
         ],
-        [
-            ('Images', archive_stats['images_count']),
-            ('Public Images', archive_stats['public_images_count']),
-            ('Annotated Images', archive_stats['annotated_images_count']),
-        ],
-        [
-            ('Studies', archive_stats['studies_count']),
-            ('Public Studies', archive_stats['public_studies_count']),
-            ('', ''),
-        ],
-        [
-            ('Annotations', archive_stats['annotations_count']),
-            ('Markups', archive_stats['markups_count']),
-            ('Responses', archive_stats['responses_count']),
-        ],
-    ]
+    }
     return render(request, 'stats/stats.html', ctx)
