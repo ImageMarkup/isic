3ed5377361d74df998c5f5b0363895fe131acab9
Brian Helba <brian.helba@kitware.com>
2022-03-03 12:26:35 -0500

    Add a build process for Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: [
+    './isic/*/templates/**/*.html',
+  ],
+  safelist: [
+    // Injected by Django, and may be referenced by CSS rules
+    'errorlist',
+  ],
+  theme: {
+    fontFamily: {
+      'sans': 'Nunito, sans-serif',
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('daisyui'),
+  ],
+  daisyui: {
+    logs: false,
+  },
+}
