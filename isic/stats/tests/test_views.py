8496adba5de9daef6c29298f7b863a426d71b6f3
Dan LaManna <dan.lamanna@kitware.com>
2022-03-04 11:59:15 -0500

    Remove stats endpoint

--- isic/stats/tests/test_views.py
+++ isic/stats/tests/test_views.py
@@ -6,9 +6,3 @@ import pytest
 def test_core_stats(client):
     r = client.get(reverse('stats/stats'))
     assert r.status_code == 200
-
-
-@pytest.mark.django_db
-def test_core_api_stats(client):
-    r = client.get(reverse('stats/api/stats'))
-    assert r.status_code == 200
