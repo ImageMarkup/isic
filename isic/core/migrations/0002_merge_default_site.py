a7fb0f982e1d90697fe5e8fa4ac48c74e9c26e1d
Brian Helba <brian.helba@kitware.com>
2022-03-20 17:06:44 -0400

    Move default_site migration to core app

---
+++ isic/core/migrations/0002_merge_default_site.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_default_site'),
+        ('core', '0001_initial_squashed'),
+    ]
+
+    operations = []
