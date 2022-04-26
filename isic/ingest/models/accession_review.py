dc67ff951bb2f0324a8025a95d14954cfc81bf1f
Dan LaManna <dan.lamanna@kitware.com>
2022-04-26 17:01:56 -0400

    Fix accessionreview constraint

--- isic/ingest/models/accession_review.py
+++ isic/ingest/models/accession_review.py
@@ -7,7 +7,7 @@ from .accession import Accession
 
 
 class AccessionReview(CreationSortedTimeStampedModel):
-    accession = models.OneToOneField(Accession, on_delete=models.PROTECT, related_name='review')
+    accession = models.OneToOneField(Accession, on_delete=models.CASCADE, related_name='review')
     creator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     reviewed_at = models.DateTimeField()
     value = models.BooleanField()
