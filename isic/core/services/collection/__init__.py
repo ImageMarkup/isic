51b49195f6e8f98b2eb861eaccf66cdf49a75590
Dan LaManna <dan.lamanna@kitware.com>
2022-05-04 15:01:02 -0400

    Add scripts for merging cohorts and collections

--- isic/core/services/collection/__init__.py
+++ isic/core/services/collection/__init__.py
@@ -1,11 +1,23 @@
 import logging
+from typing import Iterable
 
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.db import transaction
 from django.db.models.aggregates import Count
+from django.db.models.query_utils import Q
 
 from isic.core.models.collection import Collection
+from isic.core.models.doi import Doi
+from isic.core.services.collection.image import collection_add_images
+from isic.studies.models import Study
 
 logger = logging.getLogger(__name__)
+
+
+def _require_unlocked_collection(collection: Collection) -> None:
+    if Collection.objects.filter(pk=collection.pk, locked=True).exists():
+        raise ValidationError("Can't modify the collection, it's locked.")
 
 
 def collection_create(*, creator: User, name: str, description: str, public: bool):
@@ -14,12 +26,27 @@ def collection_create(*, creator: User, name: str, description: str, public: boo
     )
 
 
-def collection_update(collection: Collection, **fields):
+def collection_update(collection: Collection, ignore_lock: bool = False, **fields):
+    if not ignore_lock:
+        _require_unlocked_collection(collection)
+
     for field, value in fields.items():
         setattr(collection, field, value)
 
     collection.full_clean()
     return collection.save()
+
+
+def collection_delete(*, collection: Collection, ignore_lock: bool = False) -> None:
+    if not ignore_lock:
+        _require_unlocked_collection(collection)
+
+    if collection.studies.exists():
+        raise ValidationError('Collections with derived studies cannot be deleted.')
+    elif collection.has_doi:
+        raise ValidationError('Collections with DOIs cannot be deleted.')
+
+    collection.delete()
 
 
 def collection_get_creators_in_attribution_order(*, collection: Collection) -> list[str]:
@@ -40,3 +67,36 @@ def collection_get_creators_in_attribution_order(*, collection: Collection) -> l
     creators = sorted(creators, key=lambda x: 1 if x == 'Anonymous' else 0)
 
     return creators
+
+
+def collection_merge(
+    *, dest_collection: Collection, other_collections: Iterable[Collection]
+) -> None:
+    """
+    Merge one or more collections into dest_collection.
+
+    Note that this method should almost always be used with cohort_merge. Merging
+    collections or cohorts with relationships to the other would put the system in
+    an unexpected state otherwise.
+    """
+    from_collection_filter = Q(collection=dest_collection) | Q(collection__in=other_collections)
+
+    if Study.objects.filter(from_collection_filter).exists():
+        raise ValidationError('Collections with derived studies cannot be merged.')
+    elif Doi.objects.filter(from_collection_filter).exists():
+        raise ValidationError('Collections with DOIs cannot be merged.')
+
+    # TODO: collection shares will need to be handled
+
+    with transaction.atomic():
+        for collection in other_collections:
+            if collection.cohort and collection.cohort != dest_collection.cohort:
+                logger.warning(f'Abandoning cohort {collection.cohort.pk}')
+
+            collection_update(
+                collection=collection, cohort=dest_collection.cohort, ignore_lock=True
+            )
+            collection_add_images(
+                collection=dest_collection, qs=collection.images.iterator(), ignore_lock=True
+            )
+            collection_delete(collection=collection, ignore_lock=True)
