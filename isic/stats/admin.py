cc3ada6c313585d78f26430bb0d690fb2a3a1952
Dan LaManna <dan.lamanna@kitware.com>
2022-05-16 18:39:51 -0400

    Add image downloads admin filter

--- isic/stats/admin.py
+++ isic/stats/admin.py
@@ -4,12 +4,32 @@ from django.db.models.expressions import F
 from isic.stats.models import ImageDownload
 
 
+class DownloadedWithIsicCliFilter(admin.SimpleListFilter):
+    title = 'User Agent'
+    parameter_name = 'User Agent'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('isic_cli', 'ISIC CLI'),
+            ('other', 'Other'),
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == 'isic_cli':
+            return queryset.filter(user_agent__startswith='isic-cli')
+        elif value == 'other':
+            return queryset.exclude(user_agent__startswith='isic-cli')
+        return queryset
+
+
 @admin.register(ImageDownload)
 class ImageDownloadAdmin(admin.ModelAdmin):
     list_display = ['isic_id', 'download_time', 'ip_address']
     list_select_related = ['image']
     search_fields = ['image__isic__id', 'ip_address', 'user_agent']
     search_help_text = 'Search by ISIC ID, IP Address, or User Agent.'
+    list_filter = [DownloadedWithIsicCliFilter]
 
     autocomplete_fields = ['image']
 
