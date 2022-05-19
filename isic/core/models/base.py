d0bff64dc8d57478c92049bb01ffcf5c279c4e12
Dan LaManna <dan.lamanna@kitware.com>
2022-05-19 11:40:46 -0400

    Allow for regex redirect uris in oauth apps

--- isic/core/models/base.py
+++ isic/core/models/base.py
@@ -1,6 +1,10 @@
+import re
+
+from django.conf import settings
 from django.db import models
 from django_extensions.db.fields import CreationDateTimeField
 from django_extensions.db.models import TimeStampedModel
+from oauth2_provider.models import AbstractApplication, redirect_to_uri_allowed
 
 
 class CreationSortedTimeStampedModel(TimeStampedModel):
@@ -18,3 +22,27 @@ class CopyrightLicense(models.TextChoices):
     # These 2 require attribution
     CC_BY = 'CC-BY', 'CC-BY'
     CC_BY_NC = 'CC-BY-NC', 'CC-BY-NC'
+
+
+class IsicOAuthApplication(AbstractApplication):
+    def redirect_uri_allowed(self, uri):
+        regex_redirect_uris = [
+            redirect_uri
+            for redirect_uri in self.redirect_uris.split()
+            if redirect_uri.startswith('^')
+        ]
+        non_regex_redirect_uris = [
+            redirect_uri
+            for redirect_uri in self.redirect_uris.split()
+            if not redirect_uri.startswith('^')
+        ]
+
+        if settings.ISIC_OAUTH_ALLOW_REGEX_REDIRECT_URIS:
+            matches_regex_uri = any(
+                re.match(redirect_uri_regex, uri) for redirect_uri_regex in regex_redirect_uris
+            )
+
+            if matches_regex_uri:
+                return True
+
+        return redirect_to_uri_allowed(uri, non_regex_redirect_uris)
---
+++ isic/core/tests/test_isic_oauth_app.py
@@ -0,0 +1,32 @@
+from oauth2_provider.models import get_application_model
+import pytest
+
+from isic.core.models.base import IsicOAuthApplication
+
+
+@pytest.fixture
+def oauth_allow_regex_redirect_uris(settings):
+    settings.ISIC_OAUTH_ALLOW_REGEX_REDIRECT_URIS = True
+    yield
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'uri,allowed_uris,allowed',
+    [
+        ['http://foo.com', 'http://foo.com', True],
+        ['http://bar.com', 'http://foo.com', False],
+        ['http://bar.com', 'http://foo.com ^http://bar.com$', True],
+        ['http://bar5.com', 'http://foo.com ^http://bar[0-9]\\.com$', True],
+    ],
+)
+def test_redirect_uri_allowed(oauth_allow_regex_redirect_uris, user, uri, allowed_uris, allowed):
+    app = IsicOAuthApplication.objects.create(
+        name='Test Application',
+        redirect_uris=allowed_uris,
+        user=user,
+        client_type=get_application_model().CLIENT_CONFIDENTIAL,
+        authorization_grant_type=get_application_model().GRANT_AUTHORIZATION_CODE,
+    )
+
+    assert app.redirect_uri_allowed(uri) == allowed
