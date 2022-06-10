4425182640a8cb0d4626c5b60a8856150c374a60
Dan LaManna <dan.lamanna@kitware.com>
2022-06-10 10:24:05 -0400

    Use study_create where possible

--- isic/studies/services/__init__.py
+++ isic/studies/services/__init__.py
@@ -3,17 +3,53 @@ from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.db.models.query import QuerySet
 
+from isic.core.models.collection import Collection
+from isic.core.services.collection import collection_lock
 from isic.studies.models import Study, StudyTask
+
+
+def _validate_study(*, study: Study) -> None:
+    if study.public and not study.collection.public:
+        raise ValidationError("Can't make a study public with a private collection.")
+
+
+def study_create(
+    *,
+    creator: User,
+    owners,
+    attribution: str,
+    name: str,
+    description: str,
+    collection: Collection,
+    public: bool,
+) -> Study:
+    study = Study(
+        creator=creator,
+        attribution=attribution,
+        name=name,
+        description=description,
+        collection=collection,
+        public=public,
+    )
+    study.full_clean()
+
+    _validate_study(study=study)
+
+    with transaction.atomic():
+        study.save()
+        study.owners.set(owners)
+        collection_lock(collection=collection)
+
+    return study
 
 
 def study_update(*, study: Study, **fields):
     for field, value in fields.items():
         setattr(study, field, value)
 
-    if study.public and not study.collection.public:
-        raise ValidationError("Can't make a study public with a private collection.")
-
     study.full_clean()
+
+    _validate_study(study=study)
 
     return study.save()
 
