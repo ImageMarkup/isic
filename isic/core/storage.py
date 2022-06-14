1f6664566067a769497db298d12a29e3357741bc
Dan LaManna <dan.lamanna@kitware.com>
2022-06-14 12:41:31 -0400

    Add cacheable signing URLs

--- isic/core/storage.py
+++ isic/core/storage.py
@@ -1,5 +1,41 @@
+from datetime import datetime, timedelta
+from urllib.parse import urlencode
 from uuid import uuid4
+
+from django.utils.encoding import filepath_to_uri
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
 def generate_upload_to(instance, filename) -> str:
     return str(uuid4())
+
+
+class CacheableCloudFrontStorage(S3Boto3Storage):
+    @staticmethod
+    def next_expiration_time(now=None):
+        # returns a time > 6 days but <= 7.
+        now = now if now else datetime.utcnow()
+        return now.replace(second=0, microsecond=0, minute=0, hour=0) + timedelta(days=7)
+
+    # This is copied from upstream with minor modifications, subclassing in a cleaner way wasn't
+    # possible.
+    def url(self, name, parameters=None, expire=None, http_method=None):
+        assert expire is None and http_method is None, 'Arguments not supported by custom storage.'
+
+        # Preserve the trailing slash after normalizing the path.
+        name = self._normalize_name(self._clean_name(name))
+        params = parameters.copy() if parameters else {}
+
+        if self.custom_domain:
+            url = '{}//{}/{}{}'.format(
+                self.url_protocol,
+                self.custom_domain,
+                filepath_to_uri(name),
+                f'?{urlencode(params)}' if params else '',
+            )
+
+            if self.querystring_auth and self.cloudfront_signer:
+                expiration = self.next_expiration_time()
+                return self.cloudfront_signer.generate_presigned_url(url, date_less_than=expiration)
+
+            return url
---
+++ isic/core/tests/test_storage.py
@@ -0,0 +1,15 @@
+from datetime import timedelta
+
+from django.utils import timezone
+from hypothesis import given, strategies as st
+
+from isic.core.storage import CacheableCloudFrontStorage
+
+
+@given(t_now=st.datetimes(timezones=st.just(None)))
+def test_expiration_time(t_now):
+    t_now = timezone.make_aware(t_now)
+    t_next = CacheableCloudFrontStorage.next_expiration_time(t_now)
+    expires_in = t_next - t_now
+    assert expires_in > timedelta(days=6)
+    assert expires_in <= timedelta(days=7)
