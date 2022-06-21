bec1312a36b770162c91210f2df98d6444468bbc
Dan LaManna <dan.lamanna@kitware.com>
2022-06-21 18:58:06 -0400

    Add test for downloading study responses

--- isic/studies/tests/test_permissions.py
+++ isic/studies/tests/test_permissions.py
@@ -38,27 +38,6 @@ def private_study_and_owner(private_study):
 
 
 @pytest.fixture
-def private_study_with_responses(study_factory, user_factory, response_factory):
-    # create a scenario for testing that a user can only see their responses and
-    # not another annotators.
-    study = study_factory(public=False)
-    u1, u2 = user_factory(), user_factory()
-    response_factory(
-        annotation__annotator=u1,
-        annotation__study=study,
-        annotation__task__annotator=u1,
-        annotation__task__study=study,
-    )
-    response_factory(
-        annotation__annotator=u2,
-        annotation__study=study,
-        annotation__task__annotator=u2,
-        annotation__task__study=study,
-    )
-    return study, u1, u2
-
-
-@pytest.fixture
 def study_task_with_user(study_task_factory, user_factory):
     u = user_factory()
     study_task = study_task_factory(annotator=u)
