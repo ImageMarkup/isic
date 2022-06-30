8753b696c6cb71a2cd8500400ceb3257df588d43
Dan LaManna <dan.lamanna@kitware.com>
2022-06-30 18:16:11 -0400

    Migrate disposition of all relevant files

--- isic/ingest/management/commands/migrate_content_disposition.py
+++ isic/ingest/management/commands/migrate_content_disposition.py
@@ -1,26 +1,40 @@
 import boto3
 from django.conf import settings
-from django.db import transaction
 import djclick as click
 
+from isic.core.models import Segmentation
 from isic.ingest.models import Accession
+from isic.studies.models import Markup
 
 client = boto3.client('s3')
 
 
+def make_attachment(key):
+    client.copy_object(
+        Bucket=settings.AWS_STORAGE_BUCKET_NAME,
+        Key=key,
+        CopySource=f'{settings.AWS_STORAGE_BUCKET_NAME}/{key}',
+        ContentDisposition='attachment',
+        # Content-Disposition is metadata, so it needs to be "replaced"
+        MetadataDirective='REPLACE',
+    )
+
+
 @click.command()
 def migrate_content_disposition():
-    with transaction.atomic():
-        accessions = Accession.objects.ingested()
+    accessions = Accession.objects.ingested()
+    with click.progressbar(accessions.iterator(), length=accessions.count()) as bar:
+        for accession in bar:
+            make_attachment(accession.blob.name)
+            make_attachment(accession.original_blob.name)
+            make_attachment(accession.thumbnail_256.name)
 
-        with click.progressbar(accessions.iterator(), length=accessions.count()) as bar:
-            for accession in bar:
+    segmentations = Segmentation.objects.all()
+    with click.progressbar(segmentations.iterator(), length=segmentations.count()) as bar:
+        for segmentation in bar:
+            make_attachment(segmentation.mask.name)
 
-                client.copy_object(
-                    Bucket=settings.AWS_STORAGE_BUCKET_NAME,
-                    Key=accession.blob.name,
-                    CopySource=f'{settings.AWS_STORAGE_BUCKET_NAME}/{accession.blob.name}',
-                    ContentDisposition='attachment',
-                    # Content-Disposition is metadata, so it needs to be "replaced"
-                    MetadataDirective='REPLACE',
-                )
+    markups = Markup.objects.all()
+    with click.progressbar(markups.iterator(), length=markups.count()) as bar:
+        for markup in bar:
+            make_attachment(markup.mask.name)
