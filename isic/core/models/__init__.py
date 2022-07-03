07ae0929266e4c6774ca04bc44e9ec1b0fa81c68
Dan LaManna <dan.lamanna@kitware.com>
2022-07-03 00:34:42 -0400

    Create public group and keep it synchronized

--- isic/core/models/__init__.py
+++ isic/core/models/__init__.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 from .base import CopyrightLicense, CreationSortedTimeStampedModel, IsicOAuthApplication
 from .collection import Collection
 from .doi import Doi
@@ -19,3 +23,18 @@ __all__ = [
     'Segmentation',
     'SegmentationReview',
 ]
+
+
+@receiver(post_save, sender=User)
+def add_or_remove_groups(sender: type[User], instance: User, created: bool, **kwargs):
+    from django.contrib.auth.models import Group
+
+    if created:
+        instance.groups.add(Group.objects.get(name='Public'))
+    else:
+        isic_staff_group = Group.objects.get(name='ISIC Staff')
+
+        if instance.is_staff:
+            instance.groups.add(isic_staff_group)
+        else:
+            instance.groups.remove(isic_staff_group)
