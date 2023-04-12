6b6692badbfc8e8d1a82c07c80eeefd33aacbeb3
Dan LaManna <dan.lamanna@kitware.com>
2023-04-12 11:14:31 -0400

    Improve performance of import-db.sh

--- dev/import-db.sh
+++ dev/import-db.sh
@@ -5,8 +5,10 @@ export HEROKU_APP=isic
 
 docker-compose exec postgres bash -c "PGPASSWORD=postgres dropdb --host localhost --username postgres --if-exists django && createdb --host localhost --username postgres django"
 
+# public.stats_imagedownload is large and not usually needed
 docker-compose exec postgres bash -c \
 "pg_dump --format=custom --no-privileges $(heroku config:get DATABASE_URL)\
+         --exclude-table-data public.stats_imagedownload \
  | PGPASSWORD=postgres pg_restore --host localhost --username postgres --format=custom --no-privileges --no-owner --dbname=django"
 
 docker-compose restart postgres
