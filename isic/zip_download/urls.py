e0abd371c7ef10b735e535956792b9518784caab
Dan LaManna <dan.lamanna@kitware.com>
2023-07-20 13:32:05 -0400

    Add zip file license download endpoint

--- isic/zip_download/urls.py
+++ isic/zip_download/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from isic.zip_download.api import (
     create_zip_download_url,
     zip_file_attribution_file,
+    zip_file_license_file,
     zip_file_listing,
     zip_file_metadata_file,
 )
@@ -27,5 +28,10 @@ urlpatterns = [
         "api/v2/zip-download/attribution-file/",
         zip_file_attribution_file,
         name="zip-download/api/attribution-file",
+    ),
+    path(
+        "api/v2/zip-download/license-file/<str:license_type>/",
+        zip_file_license_file,
+        name="zip-download/api/license-file",
     ),
 ]
