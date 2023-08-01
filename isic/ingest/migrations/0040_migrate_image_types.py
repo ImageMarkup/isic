969e23e2a8aa7464ce329f3a5ff313f5061faecf
Dan LaManna <dan.lamanna@kitware.com>
2023-08-01 17:14:09 -0400

    Fix migration typo

--- isic/ingest/migrations/0040_migrate_image_types.py
+++ isic/ingest/migrations/0040_migrate_image_types.py
@@ -11,13 +11,13 @@ def migrate_image_types(apps, schema_editor):
         clinical_qs = Accession.objects.filter(metadata__image_type="clinical").select_for_update()
 
         for accession in clinical_qs.iterator():
-            accession.metadata["image_type"] = "clinical close-up"
+            accession.metadata["image_type"] = "clinical: close-up"
             objs.append(accession)
 
         overview_qs = Accession.objects.filter(metadata__image_type="overview").select_for_update()
 
         for accession in overview_qs.iterator():
-            accession.metadata["image_type"] = "clinical overview"
+            accession.metadata["image_type"] = "clinical: overview"
             objs.append(accession)
 
         Accession.objects.bulk_update(objs, ["metadata"], batch_size=100)
