0d4019a03009e94288f033e00a387c7b3e86cc03
Zach Mullen <zach.mullen@kitware.com>
2023-08-18 20:46:37 -0400

    Convert images endpoints to django-ninja

--- isic/core/tests/test_api_permissions.py
+++ isic/core/tests/test_api_permissions.py
@@ -10,55 +10,55 @@ def images(image_factory):
 
 
 @pytest.mark.django_db
-def test_core_api_image_list(images, api_client, authenticated_api_client, staff_api_client):
-    for client in [api_client, authenticated_api_client]:
-        r = client.get("/api/v2/images/")
-        assert r.status_code == 200, r.data
-        assert r.data["count"] == 1
-        assert {x["public"] for x in r.data["results"]} == {True}
+def test_core_api_image_list(images, client, authenticated_client, staff_client):
+    for client_ in [client, authenticated_client]:
+        r = client_.get("/api/v2/images/")
+        assert r.status_code == 200, r.json()
+        assert r.json()["count"] == 1
+        assert {x["public"] for x in r.json()["results"]} == {True}
 
-    r = staff_api_client.get("/api/v2/images/")
-    assert r.status_code == 200, r.data
-    assert r.data["count"] == 2
-
-
-@pytest.mark.django_db
-def test_core_api_image_list_private(private_image, authenticated_api_client):
-    r = authenticated_api_client.get("/api/v2/images/")
-    assert r.status_code == 200, r.data
-    assert r.data["count"] == 0
+    r = staff_client.get("/api/v2/images/")
+    assert r.status_code == 200, r.json()
+    assert r.json()["count"] == 2
 
 
 @pytest.mark.django_db
-def test_core_api_image_list_contributed(private_image, authenticated_api_client, user):
+def test_core_api_image_list_private(private_image, authenticated_client):
+    r = authenticated_client.get("/api/v2/images/")
+    assert r.status_code == 200, r.json()
+    assert r.json()["count"] == 0
+
+
+@pytest.mark.django_db
+def test_core_api_image_list_contributed(private_image, authenticated_client, user):
     private_image.accession.cohort.contributor.owners.add(user)
 
-    r = authenticated_api_client.get("/api/v2/images/")
-    assert r.status_code == 200, r.data
-    assert r.data["count"] == 1
+    r = authenticated_client.get("/api/v2/images/")
+    assert r.status_code == 200, r.json()
+    assert r.json()["count"] == 1
 
 
 @pytest.mark.django_db
-def test_core_api_image_list_shares(private_image, authenticated_api_client, user, staff_user):
+def test_core_api_image_list_shares(private_image, authenticated_client, user, staff_user):
     private_image.shares.add(user, through_defaults={"creator": staff_user})
     private_image.save()
 
-    r = authenticated_api_client.get("/api/v2/images/")
-    assert r.status_code == 200, r.data
-    assert r.data["count"] == 1
+    r = authenticated_client.get("/api/v2/images/")
+    assert r.status_code == 200, r.json()
+    assert r.json()["count"] == 1
 
 
 @pytest.mark.django_db
-def test_core_api_image_detail(images, authenticated_api_client, staff_api_client):
+def test_core_api_image_detail(images, authenticated_client, staff_client):
     public_image_id = images[0].isic_id
     private_image_id = images[1].isic_id
 
-    r = authenticated_api_client.get(f"/api/v2/images/{public_image_id}/")
-    assert r.status_code == 200, r.data
-    r = authenticated_api_client.get(f"/api/v2/images/{private_image_id}/")
-    assert r.status_code == 404, r.data
+    r = authenticated_client.get(f"/api/v2/images/{public_image_id}/")
+    assert r.status_code == 200, r.json()
+    r = authenticated_client.get(f"/api/v2/images/{private_image_id}/")
+    assert r.status_code == 404, r.json()
 
-    r = staff_api_client.get(f"/api/v2/images/{public_image_id}/")
-    assert r.status_code == 200, r.data
-    r = staff_api_client.get(f"/api/v2/images/{private_image_id}/")
-    assert r.status_code == 200, r.data
+    r = staff_client.get(f"/api/v2/images/{public_image_id}/")
+    assert r.status_code == 200, r.json()
+    r = staff_client.get(f"/api/v2/images/{private_image_id}/")
+    assert r.status_code == 200, r.json()
