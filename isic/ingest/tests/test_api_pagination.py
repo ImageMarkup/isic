395c9b9b2d51fc85c768f8d91d9dd59493108a5f
Zach Mullen <zach.mullen@kitware.com>
2023-08-23 11:36:42 -0400

    Add test for pagination
    Because we want to treat the cursor values as opaque, it seemed simplest to do this all in one test case.

---
+++ isic/ingest/tests/test_api_pagination.py
@@ -0,0 +1,36 @@
+from urllib.parse import urlparse
+
+import pytest
+
+
+@pytest.mark.django_db
+def test_pagination(image_factory, staff_client):
+    images = [image_factory() for _ in range(2)]
+
+    resp = staff_client.get("/api/v2/images/", data={"limit": 1})
+
+    assert resp.status_code == 200, resp.json()
+    assert len(resp.json()["results"]) == 1
+    # default order is -created, so second image should be first
+    assert resp.json()["results"][0]["isic_id"] == images[1].isic_id
+    assert resp.json()["previous"] is None
+    parsed = urlparse(resp.json()["next"])
+    assert parsed.path == "/api/v2/images/"
+    assert "cursor" in parsed.query
+
+    # follow next page link
+    resp = staff_client.get(resp.json()["next"])
+    assert resp.status_code == 200, resp.json()
+    assert len(resp.json()["results"]) == 1
+    assert resp.json()["results"][0]["isic_id"] == images[0].isic_id
+    assert resp.json()["next"] is None
+    parsed = urlparse(resp.json()["previous"])
+    assert parsed.path == "/api/v2/images/"
+    assert "cursor" in parsed.query
+
+    # make sure previous links also work
+    resp = staff_client.get(resp.json()["previous"], data={"limit": 1})
+    assert resp.status_code == 200, resp.json()
+    assert len(resp.json()["results"]) == 1
+    assert resp.json()["results"][0]["isic_id"] == images[1].isic_id
+    assert resp.json()["previous"] is None
