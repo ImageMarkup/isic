93622832ea0249293c1d7816cf52b862fb209c8f
Dan LaManna <dan.lamanna@kitware.com>
2023-09-12 13:35:57 -0400

    Add patient id support to structured search

--- isic/core/tests/test_dsl.py
+++ isic/core/tests/test_dsl.py
@@ -13,6 +13,8 @@ from isic.core.dsl import parse_query
         ["isic_id:*123", Q(isic__id__endswith="123")],
         ["lesion_id:IL_123*", Q(accession__lesion__id__startswith="IL_123")],
         ["lesion_id:*123", Q(accession__lesion__id__endswith="123")],
+        ["patient_id:IP_123*", Q(accession__patient__id__startswith="IP_123")],
+        ["patient_id:*123", Q(accession__patient__id__endswith="123")],
         ['copyright_license:"CC-0"', Q(accession__copyright_license="CC-0")],
         ["diagnosis:foobar", Q(accession__metadata__diagnosis="foobar")],
         ['diagnosis:"foo bar"', Q(accession__metadata__diagnosis="foo bar")],
