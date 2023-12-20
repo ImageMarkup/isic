8a58d23311dc2ed42e07c883f5a773ee8cf926d5
Dan LaManna <danlamanna@users.noreply.github.com>
2023-12-20 09:57:48 -0500

    Revert "Add sentry cron monitoring"

--- isic/celery.py
+++ isic/celery.py
@@ -1,17 +1,8 @@
-import logging
 import os
 
-from celery import Celery, Task, signals
+from celery import Celery, Task
 import configurations.importer
-from django.conf import settings
 from django.db import transaction
-import sentry_sdk
-from sentry_sdk.integrations.celery import CeleryIntegration
-from sentry_sdk.integrations.django import DjangoIntegration
-from sentry_sdk.integrations.logging import LoggingIntegration
-from sentry_sdk.integrations.pure_eval import PureEvalIntegration
-
-from isic.core.apps import CoreConfig
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "isic.settings"
 if not os.environ.get("DJANGO_CONFIGURATION"):
@@ -33,33 +24,6 @@ app = Celery(
     config_source="django.conf:settings",
     namespace="CELERY",
 )
-
-
-@signals.celeryd_init.connect
-def init_sentry(**kwargs):
-    if hasattr(settings, "SENTRY_DSN"):
-        sentry_sdk.init(
-            # If a "dsn" is not explicitly passed, sentry_sdk will attempt to find the DSN in
-            # the SENTRY_DSN environment variable; however, by pulling it from an explicit
-            # setting, it can be overridden by downstream project settings.
-            dsn=settings.SENTRY_DSN,
-            environment=settings.SENTRY_ENVIRONMENT,
-            release=settings.SENTRY_RELEASE,
-            integrations=[
-                LoggingIntegration(level=logging.INFO, event_level=logging.WARNING),
-                DjangoIntegration(),
-                CeleryIntegration(monitor_beat_tasks=True),
-                PureEvalIntegration(),
-            ],
-            in_app_include=["isic"],
-            # Send traces for non-exception events too
-            attach_stacktrace=True,
-            # Submit request User info from Django
-            send_default_pii=True,
-            traces_sampler=CoreConfig._get_sentry_performance_sample_rate,
-            profiles_sampler=CoreConfig._get_sentry_performance_sample_rate,
-        )
-
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
