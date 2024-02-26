850533ab6155b43b3973437ac28e1fbd75792a52
Dan LaManna <dan.lamanna@kitware.com>
2024-02-26 10:11:01 -0500

    Improve pagination performance by reducing counts

--- isic/ingest/tests/test_api_pagination.py
+++ isic/ingest/tests/test_api_pagination.py
@@ -10,6 +10,7 @@ def test_pagination(image_factory, staff_client):
     resp = staff_client.get("/api/v2/images/", data={"limit": 1})
 
     assert resp.status_code == 200, resp.json()
+    assert resp.json()["count"] == 2
     assert len(resp.json()["results"]) == 1
     # default order is -created, so second image should be first
     assert resp.json()["results"][0]["isic_id"] == images[1].isic_id
@@ -21,6 +22,8 @@ def test_pagination(image_factory, staff_client):
     # follow next page link
     resp = staff_client.get(resp.json()["next"])
     assert resp.status_code == 200, resp.json()
+    # counts are not included in paginated responses
+    assert resp.json()["count"] is None
     assert len(resp.json()["results"]) == 1
     assert resp.json()["results"][0]["isic_id"] == images[0].isic_id
     assert resp.json()["next"] is None
@@ -31,6 +34,7 @@ def test_pagination(image_factory, staff_client):
     # make sure previous links also work
     resp = staff_client.get(resp.json()["previous"], data={"limit": 1})
     assert resp.status_code == 200, resp.json()
+    assert resp.json()["count"] == 2
     assert len(resp.json()["results"]) == 1
     assert resp.json()["results"][0]["isic_id"] == images[1].isic_id
     assert resp.json()["previous"] is None
