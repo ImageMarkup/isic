818f033d8c9a370a39660d8169bf7f53496bdf96
Dan LaManna <dan.lamanna@kitware.com>
2024-05-29 12:58:19 -0400

    Add COG processing for large images

--- isic/ingest/views/accession.py
+++ isic/ingest/views/accession.py
@@ -1,23 +1,7 @@
-from collections import defaultdict
-import itertools
-
-from django.contrib import messages
 from django.contrib.admin.views.decorators import staff_member_required
-from django.contrib.humanize.templatetags.humanize import intcomma
-from django.core.exceptions import ValidationError
-from django.core.paginator import Paginator
-from django.db.models import Count, Prefetch, Q
-from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls.base import reverse
 
-from isic.core.permissions import needs_object_permission
-from isic.ingest.forms import MergeCohortForm
-from isic.ingest.models import Cohort
-from isic.ingest.models.accession import Accession, AccessionStatus
-from isic.ingest.models.contributor import Contributor
-from isic.ingest.services.cohort import cohort_merge, cohort_publish_initialize
-from isic.ingest.views import make_breadcrumbs
+from isic.ingest.models.accession import Accession
 
 
 @staff_member_required
