db60e535eb9874db1affe837297421f63c3aadc7
Dan LaManna <dan.lamanna@kitware.com>
2024-06-10 12:06:51 -0400

    Add service API for sharing images

---
+++ isic/core/tests/test_image_shares.py
@@ -0,0 +1,27 @@
+import pytest
+
+from isic.core.models.image import Image
+from isic.core.permissions import get_visible_objects
+from isic.core.services.image import image_share
+
+
+@pytest.fixture()
+def private_image(image_factory):
+    return image_factory(public=False)
+
+
+@pytest.mark.django_db()
+def test_image_shares(staff_user, user, private_image):
+    assert not user.has_perm("core.view_image", private_image)
+    assert get_visible_objects(user, "core.view_image", Image.objects.all()).count() == 0
+    image_share(image=private_image, grantor=staff_user, recipient=user)
+    assert user.has_perm("core.view_image", private_image)
+    assert get_visible_objects(user, "core.view_image", Image.objects.all()).count() == 1
+
+
+@pytest.mark.django_db()
+def test_image_shares_idempotent(staff_user, user, private_image):
+    assert not user.has_perm("core.view_image", private_image)
+    image_share(image=private_image, grantor=staff_user, recipient=user)
+    image_share(image=private_image, grantor=staff_user, recipient=user)
+    assert user.has_perm("core.view_image", private_image)
