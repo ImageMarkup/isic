9f0dd7d321cd8039ed0f48b9e6d3e96a24655f25
Dan LaManna <dan.lamanna@kitware.com>
2024-06-21 13:19:48 -0400

    Add logging for django user id

--- isic/middleware.py
+++ isic/middleware.py
@@ -1,4 +1,8 @@
+import logging
+
 from ninja.operation import PathView
+
+logger = logging.getLogger(__name__)
 
 
 # See https://github.com/vitalik/django-ninja/issues/283.
@@ -15,3 +19,14 @@ class ExemptBearerAuthFromCSRFMiddleware:
         klass = getattr(view_func, "__self__", None)
         if klass and isinstance(klass, PathView):
             request._dont_enforce_csrf_checks = True  # noqa: SLF001
+
+
+class LogRequestUserMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        logger.info(f"{request.method} {request.path} user:{getattr(request.user, "pk", "none")}")  # noqa: G004
