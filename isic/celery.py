7694e6a2303ef0e2c888bc89569e08db25e5d962
Dan LaManna <dan.lamanna@kitware.com>
2024-09-24 18:06:46 -0400

    Remove composed configuration

--- isic/celery.py
+++ isic/celery.py
@@ -3,16 +3,12 @@ import os
 from celery import Celery
 import celery.app.trace
 from celery.contrib.django.task import DjangoTask
-import configurations.importer
 
 celery.app.trace.LOG_RECEIVED = """\
 Task %(name)s[%(id)s] received: (%(args)s, %(kwargs)s)\
 """
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "isic.settings"
-if not os.environ.get("DJANGO_CONFIGURATION"):
-    raise ValueError('The environment variable "DJANGO_CONFIGURATION" must be set.')
-configurations.importer.install()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "isic.settings.production")
 
 
 # Using a string config_source means the worker doesn't have to serialize
