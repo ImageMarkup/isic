acb9bcd3a9b74e257885b179b10056acfa05da57
Dan LaManna <dan.lamanna@kitware.com>
2024-10-21 17:05:14 -0400

    Reduce downtime of elasticsearch re-indexing
    This uses a temporary index to add documents to and then moves it to the
    official location. The downtime should now only be a few seconds to
    perform the move/clone operation.

--- isic/core/management/commands/populate-elasticsearch.py
+++ isic/core/management/commands/populate-elasticsearch.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 import djclick as click
 
-from isic.core.models.image import Image
-from isic.core.search import bulk_add_to_search_index, get_elasticsearch_client, maybe_create_index
+from isic.core.search import get_elasticsearch_client
+from isic.core.tasks import sync_elasticsearch_index_task
 
 
 @click.command(help="Populate the Elasticsearch index")
@@ -10,8 +10,4 @@ from isic.core.search import bulk_add_to_search_index, get_elasticsearch_client,
 def populate_elasticsearch(chunk_size):
     es = get_elasticsearch_client()
     es.indices.delete(index=settings.ISIC_ELASTICSEARCH_INDEX, ignore=[404])
-    maybe_create_index()
-    bulk_add_to_search_index(
-        Image.objects.with_elasticsearch_properties().all(), chunk_size=chunk_size
-    )
-    es.indices.refresh(index="_all")
+    sync_elasticsearch_index_task(chunk_size)
