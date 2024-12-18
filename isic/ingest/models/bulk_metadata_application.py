5f57ddaec934f8f5325f2081855710e826721832
Dan LaManna <dan.lamanna@kitware.com>
2024-12-18 11:26:29 -0500

    Add model for bulk metadata application

---
+++ isic/ingest/models/bulk_metadata_application.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class BulkMetadataApplication(models.Model):
+    """
+    Stores a record of a user's application of metadata to some set of images.
+
+    This is used to track the an application of metadata to a set of images with an
+    optional message to dictate why this metadata is being applied. It can optionally point
+    to a MetadataFile.
+    """
+
+    created = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    metadata_file = models.ForeignKey(
+        "MetadataFile",
+        on_delete=models.RESTRICT,
+        related_name="metadata_applications",
+        null=True,
+        blank=True,
+    )
+    message = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.message
