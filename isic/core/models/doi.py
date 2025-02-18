4f8959dbd87f01e8e479871e09c086329d60bee3
Dan LaManna <dan.lamanna@kitware.com>
2025-02-18 14:45:44 -0500

    Store slugs for DOIs

--- isic/core/models/doi.py
+++ isic/core/models/doi.py
@@ -5,10 +5,10 @@ from django_extensions.db.models import TimeStampedModel
 
 
 class Doi(TimeStampedModel):
-    # TODO: figure out which fields to store here
     id = models.CharField(
         max_length=30, primary_key=True, validators=[RegexValidator(r"^\d+\.\d+/\d+$")]
     )
+    slug = models.SlugField(max_length=150, unique=True)
     creator = models.ForeignKey(User, on_delete=models.RESTRICT)
 
     url = models.CharField(max_length=200)
