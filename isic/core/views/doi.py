fdfc012fcdcf258abe3c76d4272681be53e9bbc5
Dan LaManna <dan.lamanna@kitware.com>
2025-02-19 12:45:57 -0500

    Create initial DOI landing page

---
+++ isic/core/views/doi.py
@@ -0,0 +1,44 @@
+from django.shortcuts import get_object_or_404, render
+
+from isic.core.models.doi import Doi
+
+LICENSE_SHORTHAND_DESCRIPTIONS = {
+    "CC-0": "This content is free to use, modify, and share for any purpose, with no restrictions or attribution required.",  # noqa: E501
+    "CC-BY": "This content is free to use, modify, and share as long as you provide credit to the original creator.",  # noqa: E501
+    "CC-BY-NC": "This content is free to use, modify, and share for non-commercial purposes, as long as you provide credit to the original creator.",  # noqa: E501
+}
+
+LICENSE_PATHS = {
+    "CC-0": "core/licenses/cc-0.png",
+    "CC-BY": "core/licenses/cc-by.png",
+    "CC-BY-NC": "core/licenses/cc-by-nc.png",
+}
+
+
+# @cache_page(timeout=60 * 60 * 24 * 7, key_prefix="doi_detail")
+def doi_detail(request, slug):
+    doi = get_object_or_404(Doi.objects.select_related("collection"), slug=slug)
+
+    licenses = (
+        doi.collection.images.values_list("accession__copyright_license", flat=True)
+        .order_by()
+        .distinct()
+    )
+
+    context = {
+        "doi": doi,
+        "licenses": licenses,
+        "license_descriptions": LICENSE_SHORTHAND_DESCRIPTIONS,
+        "license_paths": LICENSE_PATHS,
+        "stats": {
+            "Total Images": doi.collection.images.count(),
+            "Total Lesions": doi.collection.images.values_list("accession__lesion_id", flat=True)
+            .distinct()
+            .count(),
+            "Total Patients": doi.collection.images.values_list("accession__patient_id", flat=True)
+            .distinct()
+            .count(),
+        },
+    }
+
+    return render(request, "core/doi_detail.html", context)
