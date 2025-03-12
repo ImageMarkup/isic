49e5c0890c9dc3e93739109d5ab1af11ffcad019
Dan LaManna <dan.lamanna@kitware.com>
2025-03-12 14:17:07 -0400

    Improve update-dois management command

--- isic/core/management/commands/update-dois.py
+++ isic/core/management/commands/update-dois.py
@@ -4,10 +4,18 @@ import requests
 
 from isic.core.models.doi import Doi
 from isic.core.services.collection.doi import collection_build_doi
+from isic.core.tasks import fetch_doi_citations_task, fetch_doi_schema_org_dataset_task
 
 
 @click.command(help="Update all DOIs")
 def update_dois():
+    """
+    Update all DOIs on DataCite.
+
+    This is useful after making changes to the metadata provided on DOI creation
+    (see collection_build_doi). After updating the DOIs, the citations and schema.org
+    information are re-fetched from DataCite.
+    """
     for doi in Doi.objects.iterator():
         new_doi = collection_build_doi(collection=doi.collection, doi_id=doi.id)
         r = requests.put(
@@ -20,3 +28,5 @@ def update_dois():
             click.echo(f"{doi.id} failed: {r.status_code} {r.text}")
         else:
             click.echo(f"{doi.id} succeeded")
+            fetch_doi_citations_task.delay_on_commit(doi.id)
+            fetch_doi_schema_org_dataset_task.delay_on_commit(doi.id)
