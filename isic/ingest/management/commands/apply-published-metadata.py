c93e0a7bb237a0d87004b53346736a98e8b537b2
Dan LaManna <dan.lamanna@kitware.com>
2025-03-24 12:03:30 -0400

    Allow deferring constraints in apply-published-metadata

--- isic/ingest/management/commands/apply-published-metadata.py
+++ isic/ingest/management/commands/apply-published-metadata.py
@@ -14,8 +14,8 @@ from isic.ingest.services.accession import bulk_accession_update_metadata
 @click.argument("user_id")
 @click.argument("csv_path")
 @click.argument("message")
-@transaction.atomic
-def apply_published_metadata(user_id, csv_path, message):
+@click.option("--defer-constraints", is_flag=True, help="Defer all constraints for the transaction")
+def apply_published_metadata(user_id, csv_path, message, defer_constraints):
     user = User.objects.get(pk=user_id)
 
     def rows_by_accession_id():
@@ -40,12 +40,21 @@ def apply_published_metadata(user_id, csv_path, message):
 
                     yield accession_id, row
 
-    bulk_accession_update_metadata(
-        user=user,
-        metadata=rows_by_accession_id(),
-        metadata_application_message=message,
-        ignore_image_check=True,
-        reset_review=False,
-    )
+    with transaction.atomic():
+        if defer_constraints:
+            with transaction.get_connection().cursor() as cursor:
+                # it's possible when updating metadata that the constraints are temporarily
+                # violated. an example being when updating patient ids, it's possible it
+                # could temporarily violate the "identical lesions implies idential patients"
+                # constraint and then later be corrected.
+                cursor.execute("SET CONSTRAINTS ALL DEFERRED")
+
+        bulk_accession_update_metadata(
+            user=user,
+            metadata=rows_by_accession_id(),
+            metadata_application_message=message,
+            ignore_image_check=True,
+            reset_review=False,
+        )
 
     click.secho(f"Applied CSV {csv_path} as {user.email}", fg="green")
