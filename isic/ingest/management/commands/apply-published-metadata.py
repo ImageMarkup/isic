1d1004560e394fcf627eb90b60de70c78e62818b
Dan LaManna <dan.lamanna@kitware.com>
2025-03-26 12:39:26 -0400

    Always defer constraints when applying bulk metadata updates

--- isic/ingest/management/commands/apply-published-metadata.py
+++ isic/ingest/management/commands/apply-published-metadata.py
@@ -3,7 +3,6 @@ from itertools import batched
 from pathlib import Path
 
 from django.contrib.auth.models import User
-from django.db import transaction
 import djclick as click
 
 from isic.ingest.models import Accession
@@ -14,8 +13,7 @@ from isic.ingest.services.accession import bulk_accession_update_metadata
 @click.argument("user_id")
 @click.argument("csv_path")
 @click.argument("message")
-@click.option("--defer-constraints", is_flag=True, help="Defer all constraints for the transaction")
-def apply_published_metadata(user_id, csv_path, message, defer_constraints):
+def apply_published_metadata(user_id, csv_path, message):
     user = User.objects.get(pk=user_id)
 
     def rows_by_accession_id():
@@ -40,21 +38,12 @@ def apply_published_metadata(user_id, csv_path, message, defer_constraints):
 
                     yield accession_id, row
 
-    with transaction.atomic():
-        if defer_constraints:
-            with transaction.get_connection().cursor() as cursor:
-                # it's possible when updating metadata that the constraints are temporarily
-                # violated. an example being when updating patient ids, it's possible it
-                # could temporarily violate the "identical lesions implies idential patients"
-                # constraint and then later be corrected.
-                cursor.execute("SET CONSTRAINTS ALL DEFERRED")
-
-        bulk_accession_update_metadata(
-            user=user,
-            metadata=rows_by_accession_id(),
-            metadata_application_message=message,
-            ignore_image_check=True,
-            reset_review=False,
-        )
+    bulk_accession_update_metadata(
+        user=user,
+        metadata=rows_by_accession_id(),
+        metadata_application_message=message,
+        ignore_image_check=True,
+        reset_review=False,
+    )
 
     click.secho(f"Applied CSV {csv_path} as {user.email}", fg="green")
