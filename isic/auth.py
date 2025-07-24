cb0e9bff8b07c48f436f65ea26cfa5cad8a78776
Dan LaManna <dan.lamanna@kitware.com>
2025-07-24 18:06:20 -0400

    Add PermissionedTokenAuth

--- isic/auth.py
+++ isic/auth.py
@@ -1,11 +1,41 @@
 from collections.abc import Callable
+from typing import Literal
 
+from django.http import HttpRequest
 from ninja.security import HttpBearer, django_auth
 from oauth2_provider.oauth2_backends import get_oauthlib_core
 
 from isic.core.permissions import SessionAuthStaffUser
 
 ACCESS_PERMS = ["any", "is_authenticated", "is_staff"]
+
+from allauth.idp.oidc.contrib.ninja.security import TokenAuth  # noqa: E402
+
+
+class PermissionedTokenAuth(TokenAuth):
+    def __init__(
+        self, permission: Literal["any", "is_authenticated", "is_staff"], scope: str | list | dict
+    ):
+        if permission not in ACCESS_PERMS:
+            raise ValueError(f"Invalid permission: {permission}")
+
+        super().__init__(scope)
+        self.permission = permission
+
+    def __call__(self, request: HttpRequest):
+        result = super().__call__(request)
+        if result is not None:
+            if self.permission == "any":
+                return result
+            if self.permission == "is_authenticated" and request.user.is_authenticated:
+                return result
+            if (
+                self.permission == "is_staff"
+                and request.user.is_authenticated
+                and request.user.is_staff
+            ):
+                return result
+        return None
 
 
 class OAuth2AuthBearer(HttpBearer):
@@ -39,6 +69,6 @@ class OAuth2AuthBearer(HttpBearer):
 
 
 # The lambda _: True is to handle the case where a user doesn't pass any authentication.
-allow_any: list[Callable] = [django_auth, OAuth2AuthBearer("any"), lambda _: True]
-is_authenticated = [django_auth, OAuth2AuthBearer("is_authenticated")]
-is_staff = [SessionAuthStaffUser(), OAuth2AuthBearer("is_staff")]
+allow_any: list[Callable] = [django_auth, PermissionedTokenAuth("any", scope=[]), lambda _: True]
+is_authenticated = [django_auth, PermissionedTokenAuth("is_authenticated", scope=[])]
+is_staff = [SessionAuthStaffUser(), PermissionedTokenAuth("is_staff", scope=[])]
