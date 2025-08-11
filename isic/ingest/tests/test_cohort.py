1467391dcba6df5e6be0db5352a4771a8c07cbcb
Dan LaManna <dan.lamanna@kitware.com>
2025-08-11 17:13:51 -0400

    Make cohort renames rename magic collections

--- isic/ingest/tests/test_cohort.py
+++ isic/ingest/tests/test_cohort.py
@@ -2,6 +2,8 @@ from django.core.exceptions import ValidationError
 from django.urls import reverse
 import pytest
 
+from isic.core.services.collection import collection_create
+from isic.ingest.forms import CohortForm
 from isic.ingest.services.cohort import cohort_delete
 
 
@@ -38,3 +40,35 @@ def test_cohort_detail_view_with_published_and_unpublished_accessions(
     r = staff_client.get(reverse("cohort-detail", args=[cohort.pk]))
     assert r.status_code == 200
     assert len(r.context["accessions"]) == 2
+
+
+@pytest.mark.django_db
+def test_cohort_form_updates_magic_collection_name(cohort_factory, user):
+    cohort = cohort_factory(name="foo")
+
+    magic_collection = collection_create(
+        creator=user,
+        name=f"Publish of {cohort.name}",
+        description="",
+        public=False,
+        locked=True,
+    )
+    cohort.collection = magic_collection
+    cohort.save()
+
+    assert cohort.collection.name == "Publish of foo"
+
+    form_data = {
+        "name": "bar",
+        "description": cohort.description,
+        "default_copyright_license": cohort.default_copyright_license,
+        "default_attribution": cohort.default_attribution,
+    }
+    form = CohortForm(form_data, instance=cohort)
+    assert form.is_valid()
+
+    updated_cohort = form.save()
+
+    magic_collection.refresh_from_db()
+    assert updated_cohort.name == "bar"
+    assert magic_collection.name == "Publish of bar"
