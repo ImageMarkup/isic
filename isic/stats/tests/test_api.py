75b91094aa2af2134a4b5bf2b712e60f7582bedc
Dan LaManna <dan.lamanna@kitware.com>
2025-10-30 13:36:34 -0400

    Remove search query tracking
    This has always been fairly half baked as it doesn't encompass form
    searches, zip downloads, etc.

--- isic/stats/tests/test_api.py
+++ isic/stats/tests/test_api.py
@@ -1,26 +1,8 @@
 from django.urls.base import reverse
 import pytest
 
-from isic.stats.models import SearchQuery
-
 
 @pytest.mark.django_db
 def test_api_stats(client):
     r = client.get(reverse("api:stats"))
     assert r.status_code == 200
-
-
-@pytest.mark.django_db
-@pytest.mark.parametrize(
-    ("query", "expected_count"),
-    [
-        ("", 0),
-        ("diagnosis_1:Benign AND -melanocytic:*", 1),
-    ],
-)
-def test_search_query_logging(client, query, expected_count):
-    response = client.get(reverse("api:search_images"), {"query": query})
-    assert response.status_code == 200
-    assert SearchQuery.objects.count() == expected_count
-    if expected_count > 0:
-        assert SearchQuery.objects.last().value == query
