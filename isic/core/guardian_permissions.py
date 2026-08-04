83c3d0bf999cd2a6802460617c6ae281987e522d
Anne Haley <anne.haley@kitware.com>
2026-08-04 20:09:59 +0000

    Fix group assignments in user `post_save` signal

--- isic/core/guardian_permissions.py
+++ isic/core/guardian_permissions.py
@@ -2,36 +2,30 @@ from django.contrib.auth.models import Group, User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from guardian.shortcuts import assign_perm
-from guardian.utils import get_anonymous_user
 
 from isic.core.models.image import Image, ImageShare
 from isic.ingest.models import Contributor
 
+STAFF_GROUP_NAME = "ISIC Staff"
+PUBLIC_GROUP_NAME = "Public"
 
-def initialize_groups():
-    staff_group, _ = Group.objects.get_or_create(name="Staff")
-    staff_group.user_set.add(*User.objects.filter(is_staff=True))
-    anon_user = get_anonymous_user()
-    public_group, _ = Group.objects.get_or_create(name="Public")
-    public_group.user_set.add(*User.objects.all(), anon_user)
-    for contributor in Contributor.objects.all():
-        contributor_group, _ = Group.objects.get_or_create(name=f"contributor_{contributor.id}")
-        contributor_group.user_set.add(*contributor.owners.all())
+
+def get_contributor_group_name(contributor):
+    return f"contributor_{contributor.id}"
 
 
 def assign_image_perms():
-    initialize_groups()
     all_images = Image.objects.all()
     public_images = Image.objects.filter(public=True)
-    staff_group = Group.objects.get(name="Staff")
-    public_group = Group.objects.get(name="Public")
+    staff_group = Group.objects.get(name=STAFF_GROUP_NAME)
+    public_group = Group.objects.get(name=PUBLIC_GROUP_NAME)
     assign_perm("view_image", public_group, public_images)
     assign_perm("view_image", staff_group, all_images)
     assign_perm("view_image_metadata", staff_group, all_images)
     for image_share in ImageShare.objects.all():
         assign_perm("view_image", image_share.grantee, image_share.image)
     for contributor in Contributor.objects.all():
-        contributor_group = Group.objects.get(name=f"contributor_{contributor.id}")
+        contributor_group = Group.objects.get(name=get_contributor_group_name(contributor))
         contributor_images = Image.objects.filter(accession__cohort__contributor=contributor.id)
         assign_perm("view_image", contributor_group, contributor_images)
         assign_perm("view_image_metadata", contributor_group, contributor_images)
@@ -39,21 +33,33 @@ def assign_image_perms():
 
 @receiver(post_save, sender=User)
 def user_save_assign_groups(sender, instance, created, **kwargs):
-    initialize_groups()
+    instance.groups.clear()
+
+    public_group, _ = Group.objects.get_or_create(name=PUBLIC_GROUP_NAME)
+    instance.groups.add(public_group)
+
+    if instance.is_staff:
+        staff_group, _ = Group.objects.get_or_create(name=STAFF_GROUP_NAME)
+        instance.groups.add(staff_group)
+
+    for contributor in instance.owned_contributors.all():
+        contributor_group, _  = Group.objects.get_or_create(
+            name=get_contributor_group_name(contributor)
+        )
+        instance.groups.add(contributor_group)
 
 
 @receiver(post_save, sender=Image)
 def image_save_assign_perms(sender, instance, created, **kwargs):
     if created:
-        initialize_groups()
-        staff_group = Group.objects.get(name="Staff")
+        staff_group = Group.objects.get(name=STAFF_GROUP_NAME)
         assign_perm("view_image", staff_group, instance)
         assign_perm("view_image_metadata", staff_group, instance)
         if instance.public:
-            public_group = Group.objects.get(name="Public")
+            public_group = Group.objects.get(name=PUBLIC_GROUP_NAME)
             assign_perm("view_image", public_group, instance)
         contributor = instance.accession.cohort.contributor
-        contributor_group = Group.objects.get(name=f"contributor_{contributor.id}")
+        contributor_group = Group.objects.get(name=get_contributor_group_name(contributor))
         assign_perm("view_image", contributor_group, instance)
         assign_perm("view_image_metadata", contributor_group, instance)
 
@@ -66,7 +72,7 @@ def image_share_save_assign_perms(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=Contributor)
 def contributor_save_assign_perms(sender, instance, created, **kwargs):
-    contributor_group, _ = Group.objects.get_or_create(name=f"contributor_{instance.id}")
+    contributor_group, _ = Group.objects.get_or_create(name=get_contributor_group_name(instance))
     contributor_group.user_set.add(*instance.owners.all())
     contributor_images = Image.objects.filter(accession__cohort__contributor=instance.id)
     assign_perm("view_image", contributor_group, contributor_images)
--- isic/core/models/__init__.py
+++ isic/core/models/__init__.py
@@ -1,7 +1,3 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 from .base import CopyrightLicense, CreationSortedTimeStampedModel, IsicOAuthApplication
 from .collection import Collection
 from .collection_count import CollectionCount
@@ -31,23 +27,3 @@ __all__ = [
     "SegmentationReview",
     "SupplementalFile",
 ]
-
-
-@receiver(post_save, sender=User)
-def add_or_remove_groups(
-    sender: type[User],
-    instance: User,
-    created: bool,
-    **kwargs,
-):
-    from django.contrib.auth.models import Group
-
-    if created:
-        instance.groups.add(Group.objects.get(name="Public"))
-    else:
-        isic_staff_group = Group.objects.get(name="ISIC Staff")
-
-        if instance.is_staff:
-            instance.groups.add(isic_staff_group)
-        else:
-            instance.groups.remove(isic_staff_group)
